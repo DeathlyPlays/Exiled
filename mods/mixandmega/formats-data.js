ddbd76608aace3db42a35510d60fe670df1ea15b
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-14 09:25:04 -0500

    MNM

---
+++ mods/mixandmega/formats-data.js
@@ -0,0 +1,36 @@
+'use strict';
+
+exports.BattleFormatsData = {
+	aegislash: {
+		inherit: true,
+		tier: "OU",
+	},
+	blaziken: {
+		inherit: true,
+		tier: "OU",
+	},
+	darkrai: {
+		inherit: true,
+		tier: "OU",
+	},
+	deoxysdefense: {
+		inherit: true,
+		tier: "OU",
+	},
+	deoxysspeed: {
+		inherit: true,
+		tier: "OU",
+	},
+	landorus: {
+		inherit: true,
+		tier: "OU",
+	},
+	slaking: {
+		inherit: true,
+		tier: "Uber",
+	},
+	shayminsky: {
+		inherit: true,
+		tier: "OU",
+	},
+};

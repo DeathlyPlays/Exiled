5b08c670752ec49ea63d15c0f907063b72ddf4dc
Insist <DeathlyPlays@users.noreply.github.com>
2017-03-30 11:10:27 -0500

    Shortcut command for survey answer

--- chat-plugins/survey.js
+++ chat-plugins/survey.js
@@ -171,6 +171,9 @@ function validateAnswer(room, message) {
 }
 
 exports.commands = {
+	sa: function (target, room, user) {
+		this.parse('/survey answer ' + target);
+	},
 	survey: {
 		htmlcreate: 'new',
 		create: 'new',

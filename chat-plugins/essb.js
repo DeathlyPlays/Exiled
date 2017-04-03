f0f6f4d7d453e8549e1742e7c634ccb6144febb9
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-03 10:03:35 -0500

    Update ssb command

--- chat-plugins/essb.js
+++ chat-plugins/essb.js
@@ -2,7 +2,7 @@
 let fs = require('fs');
 let monData;
 try {
-	monData = fs.readFileSync("data/cssb-data.txt").toString().split("\n\n");
+	monData = fs.readFileSync("data/ssb-data.txt").toString().split("\n\n");
 } catch (e) {
 	console.error(e);
 }
@@ -20,29 +20,30 @@ function getMonData(target) {
 }
 
 exports.commands = {
-	capssb: 'cssb',
-	cssb: function (target, room, user) {
+	essb: 'ssb',
+	ssb: function (target, room, user) {
 		if (!this.runBroadcast()) return false;
 		if (!target || target === 'help') return this.parse('/help sssb');
-		if (target === 'credits') return this.parse('/sssbcredits');
+		if (target === 'credits') return this.parse('/ssbcredits');
 		let targetData = getMonData(toId(target));
 		if (!targetData) return this.errorReply("The staffmon '" + toId(target) + "' could not be found.");
 		return this.sendReplyBox(targetData);
 	},
 
-	capssbhelp: 'cssbhelp',
-	cssbhelp: function (target, room, user) {
+	essbhelp: 'ssbhelp',
+	ssbhelp: function (target, room, user) {
 		if (!this.runBroadcast()) return;
-		return this.sendReplyBox("/sssb [staff member\'s name] - displays data for a staffmon\'s movepool, custom move, and custom ability.");
+		return this.sendReplyBox("/ssb [staff member\'s name] - displays data for a staffmon\'s movepool, custom move, and custom ability.");
 	},
 
-	capssbcredits: 'cssbcredits',
-	cssbcredits: function (target, room, user) {
+	essbcredits: 'ssbcredits',
+	ssbcredits: function (target, room, user) {
 		if (!this.runBroadcast()) return false;
 		this.sendReplyBox(
-			"<center><b>CAP Super Staff Bros Credits:</b></center>" +
-			"<b>DeathlyPlays</b> -Programming, Testing, Concepts, Organizing, Balancing.<br />" +
-			"<b>CAP Auth</b> - Participation and support in helping to complete CSSB."
+			"<center><b>Exiled Super Staff Bros Credits:</b></center>" +
+			"<b>Insist</b> -Programming, Testing, Concepts, Organizing, Balancing.<br />" +
+			"<b>Stellation</b> - Bug fixes, programming.<br />",
+			"<b>Exiled Server Auth</b> - Participation and support in helping to complete CSSB."
 		);
 	},
 };

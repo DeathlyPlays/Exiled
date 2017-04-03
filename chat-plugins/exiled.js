58a1ba60111f42e15a0b4349ddf5b66916da67da
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-03 10:47:23 -0500

    Update exiled.js

--- chat-plugins/exiled.js
+++ chat-plugins/exiled.js
@@ -7,6 +7,8 @@ const moment = require('moment');
 const nani = require('nani').init("niisama1-uvake", "llbgsBx3inTdyGizCPMgExBVmQ5fU");
 const Autolinker = require('autolinker');
 let request = require('request');
+let path = require('path');
+let selectors;
 
 let amCache = {
 	anime: {},
@@ -83,6 +85,18 @@ global.isDev = function (user) {
 	if (dev === 1) return true;
 	return false;
 };
+
+function writeIconCSS() {
+	fs.appendFile('config/custom.css', selectors);
+}
+
+function logMoney(message) {
+	if (!message) return;
+	let file = path.join(__dirname, '../logs/money.txt');
+	let date = "[" + new Date().toUTCString() + "] ";
+	let msg = message + "\n";
+	fs.appendFile(file, date + msg);
+}
 
 exports.commands = {
 	youtube: function (target, room, user) {
@@ -636,4 +650,24 @@ exports.commands = {
 			Rooms.global.writeChatRoomData();
 		}
 	},
+
+	seticon: function (target, room, user) {
+		if (!this.can('eval')) return this.errorReply("Access denied.");
+
+		let args = target.split(',');
+		if (args.length < 3) return this.parse('/help seticon');
+		let username = toId(args.shift());
+		let image = 'background: rgba(244, 244, 244, 0.8) url("' + args.shift().trim() + '") right no-repeat;';
+		selectors = '\n\n' + '  #' + toId(args.shift()) + '-userlist-user-' + username;
+		args.forEach(function (room) {
+			selectors += ', #' + toId(room) + '-userlist-user-' + username;
+		});
+		selectors += ' { \n' + '    ' + image +  '\n  }';
+
+		logMoney(user.name + " has set an icon to " + username + ".");
+		this.privateModCommand("(" + user.name + " has set an icon to  " + username + ")");
+		Rooms('staff').add('|raw|' + user.name + " has set an icon to " + username +  ".").update();
+		writeIconCSS();
+	},
+	seticonhelp: ["/seticon [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms."],
 };

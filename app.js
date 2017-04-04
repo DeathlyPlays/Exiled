9ca013f31746390e8c0fe3ec39452289117e3ed7
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-04 10:49:02 -0500

    Travis

--- app.js
+++ app.js
@@ -54,14 +54,13 @@ const path = require('path');
 
 try {
 	require.resolve('sockjs');
-}
-catch (e) {
+} catch (e) {
 	if (require.main !== module) throw new Error("Dependencies unmet");
 
 	let command = 'npm install --production';
 	console.log('Installing dependencies: `' + command + '`...');
 	require('child_process').spawnSync('sh', ['-c', command], {
-		stdio: 'inherit'
+		stdio: 'inherit',
 	});
 }
 
@@ -71,8 +70,7 @@ catch (e) {
 
 try {
 	require.resolve('./config/config');
-}
-catch (err) {
+} catch (err) {
 	if (err.code !== 'MODULE_NOT_FOUND') throw err; // should never happen
 
 	// Copy it over synchronously from config-example.js since it's needed before we can start the server
@@ -80,8 +78,7 @@ catch (err) {
 	fs.writeFileSync(path.resolve(__dirname, 'config/config.js'),
 		fs.readFileSync(path.resolve(__dirname, 'config/config-example.js'))
 	);
-}
-finally {
+} finally {
 	global.Config = require('./config/config');
 }
 
@@ -94,8 +91,7 @@ if (Config.watchconfig) {
 			global.Config = require('./config/config');
 			if (global.Users) Users.cacheGroupData();
 			console.log('Reloaded config/config.js');
-		}
-		catch (e) {
+		} catch (e) {
 			console.log('Error reloading config/config.js: ' + e.stack);
 		}
 	});

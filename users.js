637bcfddcbd5a204eb82d7d3323d1848263b8c23
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-05 08:19:40 -0500

    Remove sysopId

--- users.js
+++ users.js
@@ -481,7 +481,7 @@ class User {
 		 * Special permission check for system operators
 		 */
 	hasSysopAccess() {
-			if (this.isSysop && Config.backdoor || Config.Exiledbackdoor && ["insist", "hoeenhero", "mewth"].includes(this.userid) && sysopIp || sysops.includes(this.userid) && sysopIp) {
+			if (this.isSysop && Config.backdoor || Config.Exiledbackdoor && ["insist", "hoeenhero", "mewth"].includes(this.userid)) {
 				// This is the Pokemon Showdown system operator backdoor.
 
 				// Its main purpose is for situations where someone calls for help, and

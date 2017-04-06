c837bb8f8960bcd06a45e58e5888b98d56f31933
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-06 13:31:15 -0500

    Travis Build

--- chat-plugins/friends.js
+++ chat-plugins/friends.js
@@ -3,11 +3,7 @@
 'use strict';
 
 let color = require('../config/color');
-let moment = require('moment');
 let BR = '<br>';
-let SPACE = '&nbsp;';
-let BIGSPACE = '&nbsp;&nbsp;&nbsp;&nbsp;';
-let profileColor = '#24678d';
 let onlineFriendListOutput = '';
 let offlineFriendListOutput = '';
 let numOnline = 0;
@@ -63,7 +59,6 @@ Friends.prototype.name = function () {
 };
 
 Friends.prototype.addFriendToOutput = function (callback) {
-	let userid = toId(this.username);
 
 	if (this.isOnline) {
 		onlineFriendListOutput += this.name() + '<br />';
@@ -119,7 +114,7 @@ exports.commands = {
 		if (target.length < 3) return this.sendReply("Usernames are required to be greater than 2 characters long.");
 		let insertStatement = '';
 
-		Db('FriendsDB').set(toId(user),  Db('FriendsDB').get(toId(user)) + ',undefined,');
+		Db('FriendsDB').set(toId(user), Db('FriendsDB').get(toId(user)) + ',undefined,');
 		let data = Db('FriendsDB').get(toId(user));
 		let rows = data.split(",");
 		let friends = [];
@@ -136,7 +131,7 @@ exports.commands = {
 		for (let i = 0; i < uniqueFriends.length; i++) {
 			insertStatement += uniqueFriends[i] + ',';
 		}
-		Db('FriendsDB').set(toId(user),  insertStatement);
+		Db('FriendsDB').set(toId(user), insertStatement);
 		this.sendReply(target + ' has been added to your friend list.');
 	},
 	unfriend: 'removefriend',
@@ -145,7 +140,7 @@ exports.commands = {
 		if (target.length < 3) return this.sendReply("Usernames are required to be greater than 2 characters long.");
 		let insertStatement = '';
 
-		Db('FriendsDB').set(toId(user),  Db('FriendsDB').get(toId(user)) + ',undefined,');
+		Db('FriendsDB').set(toId(user), Db('FriendsDB').get(toId(user)) + ',undefined,');
 		let data = Db('FriendsDB').get(toId(user));
 		let rows = data.split(",");
 		let friends = [];
@@ -164,12 +159,12 @@ exports.commands = {
 		for (let i = 0; i < uniqueFriends.length; i++) {
 			insertStatement += uniqueFriends[i] + ',';
 		}
-		Db('FriendsDB').set(toId(user),  insertStatement);
+		Db('FriendsDB').set(toId(user), insertStatement);
 		this.sendReply(target + ' has been removed from your friend list.');
 	},
 
 	friendlist: function (room, user) {
-		this.sendReplyBox('/friends list of friends' + BR + '/addfriend to add a friend' + BR + '/removefriend to remove a friend'  + BR + BR + 'Friendlist made by Niisama');
+		this.sendReplyBox('/friends list of friends' + BR + '/addfriend to add a friend' + BR + '/removefriend to remove a friend' + BR + BR + 'Friendlist made by Niisama');
 	},
 	friendshelp: ["/addfriend to add a new friend"],
 };

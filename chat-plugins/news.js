4f0aad008865902b8c596c623e1152989c1cff1d
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-06 22:35:54 -0500

    Use HH's news system
    Eh, it has functioning toggle e.e

--- chat-plugins/news.js
+++ chat-plugins/news.js
@@ -1,100 +1,106 @@
+/*
+* News System for SpacialGaze
+* Credits: Lord Haji, HoeenHero
+*/
+
 'use strict';
 
-const fs = require('fs');
-const moment = require('moment');
-const hashColor = require('../config/color.js');
-
-Exiled.userData = Object.create(null);
-
-function loadUserData() {
-    fs.readFile('config/exiledusers.json', 'utf8', function (err, file) {
-        if (err) return;
-        Exiled.userData = JSON.parse(file);
-    });
+function generateNews(user) {
+	let newsData, newsDisplay = [];
+	user = toId(user);
+	Db('news').keys().forEach(announcement => {
+		newsData = Db('news').get(announcement);
+		newsDisplay.push(`<h4>${announcement}</h4>${newsData[1]}<br /><br />—${Exiled.nameColor(newsData[0], true)} <small>on ${newsData[2]}</small>`);
+	});
+	return newsDisplay;
 }
-loadUserData();
 
-try {
-    Object.assign(Exiled, {
-
-        generateNews: function () {
-            let lobby = Rooms('lobby');
-            if (!lobby) return false;
-            if (!lobby.news || Object.keys(lobby.news).length < 0) return false;
-            if (!lobby.news) lobby.news = {};
-            let news = lobby.news,
-                newsDisplay = [];
-            Object.keys(news).forEach(announcement => {
-                newsDisplay.push(`<h4>${announcement}</h4>${news[announcement].desc}<br /><br /><strong>—<font color="${hashColor(news[announcement].by)}">${news[announcement].by}</font></strong> on ${moment(news[announcement].posted).format("MMM D, YYYY")}`);
-            });
-            return newsDisplay;
-        },
-        newsDisplay: function (user) {
-            if (!Users(user)) return false;
-            let newsDis = this.generateNews();
-            if (newsDis.length === 0) return false;
-
-            if (newsDis.length > 0) {
-                newsDis = newsDis.join('<hr>');
-                return Users(user).send(`|pm|~Exiled News|${Users(user).getIdentity()}|/raw ${newsDis}`);
-            }
-        },
-    });
+function hasSubscribed(user) {
+	if (typeof user === 'object') user = user.userid;
+	if (Db('NewsSubscribers').has(toId(user))) return true;
+	return false;
 }
-catch (e) {
-    let staff = Rooms('staff');
-    if (staff) staff.add(`|html|<div class="broadcast-red"><b>CUSTOM NEWS FUNCTIONALITY HAS CRASHED:</b><br />${e.stack}<br /><br /><b>Please report this to a developer.`).update();
+
+function showSubButton(user) {
+	user = toId(user);
+	let output;
+	output = "<hr><center><button class = \"button\" name=\"send\" value=\"/news " + (hasSubscribed(user) ? "unsubscribe" : "subscribe") + "\">" + (hasSubscribed(user) ? "Unsubscribe from the news" : "Subscribe to the news") + "</button></center>";
+	return output;
 }
+Exiled.showNews = function (userid, user) {
+	if (!user || !userid) return false;
+	userid = toId(userid);
+	let newsDisplay = generateNews(user);
+	if (!hasSubscribed(userid)) return false;
+	if (newsDisplay.length > 0) {
+		newsDisplay = newsDisplay.join('<hr>');
+		newsDisplay += showSubButton(userid);
+		return user.send(`|pm| Server News|${user.getIdentity()}|/raw ${newsDisplay}`);
+	}
+};
 
 exports.commands = {
-
-    news: 'serverannouncements',
-    announcements: 'serverannouncements',
-    serverannouncements: {
-        '': 'view',
-        display: 'view',
-        view: function (target, room, user) {
-            if (!Rooms('lobby') || !Rooms('lobby').news) return this.errorReply("Strange, there are no server announcements...");
-            if (!Rooms('lobby').news && Rooms('lobby')) Rooms('lobby').news = {};
-            let news = Rooms('lobby').news;
-            if (Object.keys(news).length === 0) return this.sendReply("There are currently no new server announcements at this time.");
-            return user.send('|popup||wide||html|' +
-                "<center><strong>Current server announcements:</strong></center>" +
-                Exiled.generateNews().join('<hr>')
-            );
-        },
-        delete: function (target, room, user) {
-            if (!this.can('root')) return false;
-            if (!Rooms('lobby').news) Rooms('lobby').news = {};
-            let news = Rooms('lobby').news;
-            if (!news[target]) return this.errorReply("This announcement doesn't seem to exist...");
-            delete news[target];
-            Rooms('lobby').news = news;
-            Rooms('lobby').chatRoomData.news = Rooms('lobby').news;
-            Rooms.global.writeChatRoomData();
-            this.privateModCommand(`(${user.name} deleted server announcement titled: ${target}.)`);
-        },
-        add: function (target, room, user) {
-            if (!this.can('root')) return false;
-            if (!target) return this.parse('/help serverannouncements');
-            let [title, ...message] = target.split("|");
-            title = title.trim();
-            if (!Rooms('lobby').news) Rooms('lobby').news = {};
-            let news = Rooms('lobby').news;
-            news[title] = {
-                desc: message,
-                posted: Date.now(),
-                by: user.name,
-            };
-            Rooms('lobby').news = news;
-            Rooms('lobby').chatRoomData.news = Rooms('lobby').news;
-            Rooms.global.writeChatRoomData();
-            this.privateModCommand(`(${user.name} added server announcement: ${message})`);
-        },
-    },
-    serverannouncementshelp: ["/announcements view - Views current server announcements.",
-        "/announcements delete [announcement title] - Deletes announcement with the [title]. Requires @, &, ~",
-        "/announcements add [announcement title]| [announcement desc] - Adds announcement [announcement]. Requires @, &, ~"
-    ],
-    // "/announcement toggle - Toggles getting news notifications."],
+	news: 'serverannouncements',
+	announcements: 'serverannouncements',
+	serverannouncements: {
+		'': 'view',
+		display: 'view',
+		view: function (target, room, user) {
+			if (!this.runBroadcast()) return;
+			let output = "<center><strong>Server News:</strong></center>";
+			output += generateNews().join('<hr>') + showSubButton(user.userid);
+			if (this.broadcasting) return this.sendReplyBox("<div class =\"infobox-limited\"" + output + "</div>");
+			return user.send('|popup||wide||html|' + output);
+		},
+		remove: 'delete',
+		delete: function (target, room, user) {
+			if (!this.can('ban')) return false;
+			if (!target) return this.parse('/help serverannouncements');
+			if (!Db('news').has(target)) return this.errorReply("News with this title doesn't exist.");
+			Db('news').delete(target);
+			this.privateModCommand(`(${user.name} deleted server announcement titled: ${target}.)`);
+		},
+		add: function (target, room, user) {
+			if (!this.can('ban')) return false;
+			if (!target) return this.parse('/help serverannouncements');
+			let parts = target.split(',');
+			if (parts.length < 2) return this.errorReply("Usage: /news add [title], [desc]");
+			let descArray = [];
+			if (parts.length - 2 > 0) {
+				for (let j = 0; j < parts.length; j++) {
+					if (j < 1) continue;
+					descArray.push(parts[j]);
+				}
+				parts[1] = descArray.join();
+			}
+			let title = parts[0], desc = parts[1], postedBy = user.name;
+			let d = new Date();
+			const MonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+				"July", "Aug", "Sep", "Oct", "Nov", "Dec",
+			];
+			let postTime = (MonthNames[d.getUTCMonth()] + ' ' + d.getUTCDate() + ", " + d.getUTCFullYear());
+			Db('news').set(title, [postedBy, desc, postTime]);
+			this.privateModCommand(`(${user.name} added server announcement: ${parts[0]})`);
+		},
+		subscribe: function (target, room, user) {
+			if (!user.named) return this.errorReply('You must choose a name before subscribing');
+			if (hasSubscribed(user.userid)) return this.errorReply("You are alreading subscribed to Server News.");
+			Db('NewsSubscribers').set(user.userid, true);
+			this.sendReply("You have subscribed to Server News.");
+			this.popupReply("|wide||html|You will receive Server News automatically once you connect to the Server next time.<br><hr><button class='button' name = 'send' value = '/news'>Go Back</button>");
+		},
+		unsubscribe: function (target, room, user) {
+			if (!user.named) return this.errorReply('You must choose a name before unsubscribing');
+			if (!hasSubscribed(user.userid)) return this.errorReply("You have not subscribed to Server News.");
+			Db('NewsSubscribers').delete(user.userid);
+			this.sendReply("You have unsubscribed to Server News.");
+			this.popupReply("|wide||html|You will no longer automatically receive Server News.<br><hr><button class='button' name='send' value='/news'>Go Back</button>");
+		},
+	},
+	serverannouncementshelp: ["/news view - Views current Server news.",
+		"/news delete [news title] - Deletes announcement with the [title]. Requires @, &, ~",
+		"/news add [news title], [news desc] - Adds news [news]. Requires @, &, ~",
+		"/news subscribe - Subscribes to Server News.",
+		"/news unsubscribe - Unsubscribes to Server News.",
+	],
 };

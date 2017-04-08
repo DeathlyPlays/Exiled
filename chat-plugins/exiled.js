bb3bf24c8b5b2270f2cd5f6a0ee5ae554cf6249a
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-07 20:11:53 -0500

    Travis Build

--- chat-plugins/exiled.js
+++ chat-plugins/exiled.js
@@ -46,13 +46,12 @@ function showDailyRewardAni(userid) {
 		output += "<img src='https://www.mukuru.com/media/img/icons/new_order.png' width='16' height='16'> ";
 	}
 	return output;
-};
+}
 
 let urbanCache;
 try {
 	urbanCache = JSON.parse(fs.readFileSync('../config/udcache.json', 'utf8'));
-}
-catch (e) {
+} catch (e) {
 	urbanCache = {};
 }
 
@@ -60,7 +59,7 @@ function cacheUrbanWord(word, definition) {
 	word = word.toLowerCase().replace(/ /g, '');
 	urbanCache[word] = {
 		"definition": definition,
-		"time": Date.now()
+		"time": Date.now(),
 	};
 	fs.writeFile('config/udcache.json', JSON.stringify(urbanCache));
 }
@@ -212,7 +211,7 @@ exports.commands = {
 	],
 
 	/* * * * * * * * * * * * *
-	 *  Allow various games  *
+	 *  Allow letious games  *
 	 *  to be played in      *
 	 *  unspecified rooms    *
 	 *  by Insist		     *
@@ -530,9 +529,9 @@ exports.commands = {
 			return this.sendReply("/roomdeowner - This room isn't designed for per-room moderation");
 		}
 		target = this.splitTarget(target, true);
-		var targetUser = this.targetUser;
-		var name = this.targetUsername;
-		var userid = toId(name);
+		let targetUser = this.targetUser;
+		let name = this.targetUsername;
+		let userid = toId(name);
 		if (!userid || userid === '') return this.sendReply("User '" + name + "' does not exist.");
 
 		if (room.auth[userid] !== '#') return this.sendReply("User '" + name + "' is not a room owner.");
@@ -547,12 +546,11 @@ exports.commands = {
 	},
 
 	roomleader: function (target, room, user) {
-
 		if (!room.chatRoomData) {
 			return this.sendReply("/roomowner - This room isn't designed for per-room moderation to be added");
 		}
 		target = this.splitTarget(target, true);
-		var targetUser = this.targetUser;
+		let targetUser = this.targetUser;
 
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' is not online.");
 
@@ -561,30 +559,7 @@ exports.commands = {
 
 		if (!room.auth) room.auth = room.chatRoomData.auth = {};
 
-		var name = targetUser.name;
-
-		room.auth[targetUser.userid] = '&';
-		this.addModCommand("" + name + " was appointed Room Leader by " + user.name + ".");
-		room.onUpdateIdentity(targetUser);
-		Rooms.global.writeChatRoomData();
-	},
-
-	roomleader: function (target, room, user) {
-
-		if (!room.chatRoomData) {
-			return this.sendReply("/roomowner - This room isn't designed for per-room moderation to be added");
-		}
-		target = this.splitTarget(target, true);
-		var targetUser = this.targetUser;
-
-		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' is not online.");
-
-		if (!room.founder) return this.sendReply('The room needs a room founder before it can have a room owner.');
-		if (room.founder !== user.userid && !this.can('makeroom')) return this.sendReply('/roomowner - Access denied.');
-
-		if (!room.auth) room.auth = room.chatRoomData.auth = {};
-
-		var name = targetUser.name;
+		let name = targetUser.name;
 
 		room.auth[targetUser.userid] = '&';
 		this.addModCommand("" + name + " was appointed Room Leader by " + user.name + ".");
@@ -598,9 +573,9 @@ exports.commands = {
 			return this.sendReply("/roomdeowner - This room isn't designed for per-room moderation");
 		}
 		target = this.splitTarget(target, true);
-		var targetUser = this.targetUser;
-		var name = this.targetUsername;
-		var userid = toId(name);
+		let targetUser = this.targetUser;
+		let name = this.targetUsername;
+		let userid = toId(name);
 		if (!userid || userid === '') return this.sendReply("User '" + name + "' does not exist.");
 
 		if (room.auth[userid] !== '&') return this.sendReply("User '" + name + "' is not a room leader.");
@@ -634,73 +609,6 @@ exports.commands = {
 	},
 	seticonhelp: ["/seticon [username], [image], [room 1], [room 2], etc. - Sets an icon to a user in chosen rooms."],
 
-	light: 'thunder',
-	thunder: function (target, room, user) {
-		if (user.can('broadcast', null, room)) {
-			var colors = ['#ffff00'];
-			if (!target) return this.sendReply('/thunder message');
-			var userColor = '',
-				currentDate = new Date(),
-				randomNumber = '';
-			for (var x = 0; x < user.name.length; x++) {
-				randomNumber = Math.floor(Math.random() * colors.length);
-				if (user.name.substring(x, x + 1) !== undefined) {
-					userColor += '<font color="' + colors[randomNumber] + '">' + user.name.substring(x, x + 1) + '</font>';
-				} else {
-					userColor += '<font color="' + colors[randomNumber] + '">:</font>';
-				}
-			}
-			if (target.indexOf('/me') > -1) {
-				room.add('|raw|<small><i>' + user.group + '</small><b>' + userColor + '</b>' + Chat.escapeHTML(target.substring(3, target.length)) + '</i>');
-			} else {
-				room.add('|raw|<small>' + user.group + '</small><b>' + userColor + '</b>: ' + target);
-			}
-		} else return this.errorReply('You must be voiced to use this command.');
-	},
-	flame: function (target, room, user) {
-		if (user.can('broadcast', null, room)) {
-			var colors = ['#ff0000'];
-			if (!target) return this.sendReply('/flame message');
-			var userColor = '',
-				currentDate = new Date(),
-				randomNumber = '';
-			for (var x = 0; x < user.name.length; x++) {
-				randomNumber = Math.floor(Math.random() * colors.length);
-				if (user.name.substring(x, x + 1) !== undefined) {
-					userColor += '<font color="' + colors[randomNumber] + '">' + user.name.substring(x, x + 1) + '</font>';
-				} else {
-					userColor += '<font color="' + colors[randomNumber] + '">:</font>';
-				}
-			}
-			if (target.indexOf('/me') > -1) {
-				room.add('|raw|<small><i>' + user.group + '</small><b>' + userColor + '</b>' + Chat.escapeHTML(target.substring(3, target.length)) + '</i>');
-			} else {
-				room.add('|raw|<small>' + user.group + '</small><b>' + userColor + '</b>: ' + target);
-			}
-		} else return this.errorReply('You must be voiced to use this command.');
-	},
-	rain: function (target, room, user) {
-		if (user.can('broadcast', null, room)) {
-			var colors = ['#33ccff'];
-			if (!target) return this.sendReply('/rain message');
-			var userColor = '',
-				currentDate = new Date(),
-				randomNumber = '';
-			for (var x = 0; x < user.name.length; x++) {
-				randomNumber = Math.floor(Math.random() * colors.length);
-				if (user.name.substring(x, x + 1) !== undefined) {
-					userColor += '<font color="' + colors[randomNumber] + '">' + user.name.substring(x, x + 1) + '</font>';
-				} else {
-					userColor += '<font color="' + colors[randomNumber] + '">:</font>';
-				}
-			}
-			if (target.indexOf('/me') > -1) {
-				room.add('|raw|<small><i>' + user.group + '</small><b>' + userColor + '</b>' + Chat.escapeHTML(target.substring(3, target.length)) + '</i>');
-			} else {
-				room.add('|raw|<small>' + user.group + '</small><b>' + userColor + '</b>: ' + target);
-			}
-		} else return this.errorReply('You must be voiced to use this command.');
-	},
 	anime: function (target, room, user) {
 		if (!this.runBroadcast()) return;
 		if (!target) return this.errorReply("No target.");

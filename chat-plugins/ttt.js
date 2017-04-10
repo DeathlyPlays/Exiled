98b2991c260d6aebae4f7565794c531f1c2e9f10
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-10 07:45:33 -0500

    Travis

--- chat-plugins/ttt.js
+++ chat-plugins/ttt.js
@@ -1,13 +1,15 @@
+'use strict';
+
 /* Tic Tac Toe Plugin By Digital Edge and Kewlstatics */
 
 if (!global.ttt) global.ttt = [{}, {}];
-var tttgames = global.ttt[0];
-var tttplayers = global.ttt[1];
+let tttgames = global.ttt[0];
+let tttplayers = global.ttt[1];
 
 const EXPIRATION_TIME = 90 * 1000;
 const INACTIVE_KICK_TIME = 30 * 1000;
 
-var TicTacToe = (function () {
+let TicTacToe = (function () {
 	function TicTacToe(user1, user2, gameNo) {
 		this.gameNo = gameNo;
 		this.p1 = user1;
@@ -29,7 +31,7 @@ var TicTacToe = (function () {
 			'6': 6,
 			'7': 7,
 			'8': 8,
-			'9': 9
+			'9': 9,
 		};
 		this.markedCount = 0;
 		this.phase = 'waiting';
@@ -41,7 +43,7 @@ var TicTacToe = (function () {
 		this.currentPlayer = this.players[Math.floor(Math.random() * 2)];
 		this.phase = 'started';
 		this.resetTimer();
-		var message = 'If you accidentally close out, use <em><b>/ttt open</b></em> to reopen the game.';
+		let message = 'If you accidentally close out, use <em><b>/ttt open</b></em> to reopen the game.';
 		this.updateUser(this.p1, message);
 		this.updateUser(this.p2, message);
 	};
@@ -52,13 +54,13 @@ var TicTacToe = (function () {
 	};
 
 	TicTacToe.prototype.getGrid = function (gameOver) {
-		var marked = [];
-		for (var i in this.boxes) {
+		let marked = [];
+		for (let i in this.boxes) {
 			if (typeof this.boxes[i] === 'string') marked.push(this.boxes[i]);
 			else marked.push('<button style = "height: 80px%; width: 80px; font-size: 20pt" name = "send" value = "/ttt markbox ' + i + '"><b>' + i + '</b></button>');
 		}
-		var style = 'width: 100px; height: 100px; font-size: 20pt; ';
-		var grid = '<table cellspacing = "0">' +
+		let style = 'width: 100px; height: 100px; font-size: 20pt; ';
+		let grid = '<table cellspacing = "0">' +
 			//row 1
 			'<tr><th style = "' + style + ' border-right: 3px solid; border-bottom: 3px solid;"><center>' + marked[0] + '</center></td>' +
 			'<th style = "' + style + ' border-bottom: 3px solid;"><center>' + marked[1] + '</center></th>' +
@@ -89,14 +91,14 @@ var TicTacToe = (function () {
 	};
 
 	TicTacToe.prototype.update = function () {
-		var message = '|html|<center><b>' + this.currentPlayer.name + '\'s turn!</b><br/>' + this.getGrid();
+		let message = '|html|<center><b>' + this.currentPlayer.name + '\'s turn!</b><br/>' + this.getGrid();
 		this.players.forEach(function (user) {
 			user.popup(message);
 		});
 	};
 
 	TicTacToe.prototype.updateUser = function (user, issue) {
-		var message = '|html|<center><b>' + this.currentPlayer.name + '\'s turn!</b><br>' +
+		let message = '|html|<center><b>' + this.currentPlayer.name + '\'s turn!</b><br>' +
 			this.getGrid() + (issue ? '<br>' + issue : '');
 		user.popup(message);
 	};
@@ -113,7 +115,7 @@ var TicTacToe = (function () {
 	};
 
 	TicTacToe.prototype.declareDraw = function () {
-		var message = '|html|<center><b>Draw between ' + this.p1.name + ' and ' + this.p2.name + '!</b><br>' + this.getGrid(true);
+		let message = '|html|<center><b>Draw between ' + this.p1.name + ' and ' + this.p2.name + '!</b><br>' + this.getGrid(true);
 		this.players.forEach(function (user) {
 			user.popup(message);
 		});
@@ -121,7 +123,7 @@ var TicTacToe = (function () {
 	};
 
 	TicTacToe.prototype.declareWinner = function () {
-		var message = '|html|<center><b>' + this.currentPlayer.name + ' has won the game!</b><br/>' + this.getGrid(true);
+		let message = '|html|<center><b>' + this.currentPlayer.name + ' has won the game!</b><br/>' + this.getGrid(true);
 		this.players.forEach(function (user) {
 			user.popup(message);
 		});
@@ -151,7 +153,7 @@ var TicTacToe = (function () {
 	return TicTacToe;
 })();
 
-var cmds = {
+let cmds = {
 	'': 'help',
 	help: function (target, room, user) {
 		if (!this.canBroadcast()) return;
@@ -169,22 +171,22 @@ var cmds = {
 	play: 'c',
 	c: function (target, room, user, connection, cmd) {
 		if (!target || !target.trim()) return this.sendReply('|html|/ttt ' + cmd + ' <em>User</em> - Challenges a user to a game of Tic-Tac-Toe.');
-		var targetUser = (Users.get(target) ? Users.get(target).name : target);
+		let targetUser = (Users.get(target) ? Users.get(target).name : target);
 		target = Users.get(target);
 		if (!target || !target.connected) return this.sendReply('User ' + targetUser + ' is offline.');
 		if (user.userid === target.userid) return this.sendReply('You can\'t play Tic-Tac-Toe with yourself!');
 		if (user.userid in tttplayers) {
-			var game = tttgames[tttplayers[user.userid]];
+			let game = tttgames[tttplayers[user.userid]];
 			if (game.phase === 'waiting') return this.sendReply('You have already requested ' + game.checkPlayer(user) + ' to a game of Tic-Tac-Toe. Wait for their response.');
 			if (game.checkPlayer(target)) return this.sendReply('You are already playing Tic-Tac-Toe with ' + target.name + '!');
 			return this.sendReply('You are already playing Tic-Tac-Toe with another user. You cannot ' + target.name + ' a request.');
 		}
 		if (target.userid in tttplayers) {
-			var game = tttgames[tttplayers[target.userid]];
+			let game = tttgames[tttplayers[target.userid]];
 			if (game.checkPlayer(user)) return this.sendReply(game.checkPlayer(user) + ' has already sent you a request...');
 			return this.sendReply(target.name + ' has already asked someone else for a game of Tic-Tac-Toe.');
 		}
-		for (var i in tttgames) {
+		for (let i in tttgames) {
 			if (tttgames[i].checkPlayer(user)) return this.sendReply('You were sent a game request by ' + tttgames[i].checkPlayer(user) + '. First respond to that request before challenging someone else.');
 			if (tttgames[i].checkPlayer(target)) return this.sendReply(target.name + ' has already challenged someone else to a game of Tic-Tac-Toe. Either wait for them to finish, or play with someone else.');
 		}
@@ -192,15 +194,15 @@ var cmds = {
 			'<button name = "send" value = "/ttt accept ' + user.userid + '">Accept</button> <button name = "send" value = "/ttt decline ' + user.userid + '">Decline</button>'
 		);
 		user.send('|pm|' + target.getIdentity() + '|' + user.getIdentity() + '|/html You have challenged ' + target.getIdentity() + ' to a game of Tic-Tac-Toe. Waiting for their response...');
-		var gameId = tttplayers[user.userid] = (Object.keys(tttgames).length ? Object.keys(tttgames).length - 1 : 0);
+		let gameId = tttplayers[user.userid] = (Object.keys(tttgames).length ? Object.keys(tttgames).length - 1 : 0);
 		tttgames[gameId] = new TicTacToe(user, target, gameId);
 	},
 
 	acc: 'accept',
 	accept: function (target, room, user, connection, cmd) {
 		if (!target || !target.trim()) return this.sendReply('|html|/ttt ' + cmd + ' <em>User</em> - Accepts a Tic-Tac-Toe challenge from a user.');
-		var game = tttgames[tttplayers[user.userid]];
-		var targetUser = (Users.get(target) ? Users.get(target).name : target);
+		let game = tttgames[tttplayers[user.userid]];
+		let targetUser = (Users.get(target) ? Users.get(target).name : target);
 		target = Users.get(target);
 		if (!target || !target.connected) return this.sendReply('User ' + targetUser + ' is offline.');
 		if (user.userid in tttplayers) {
@@ -211,7 +213,7 @@ var cmds = {
 		if (user.userid === target.userid) return this.sendReply('You can\'t accept a challenge from yourself!');
 		if (!(target.userid in tttplayers)) return this.sendReply(target.name + ' has not challenged you to a game of Tic-Tac-Toe.');
 
-		this.sendReply('|html|If you accidentally close out the game screen, use <b>/ttt open</b> to reopen it.')
+		this.sendReply('|html|If you accidentally close out the game screen, use <b>/ttt open</b> to reopen it.');
 		game = tttgames[tttplayers[target.userid]];
 		if (game.p2.userid !== user.userid) return this.sendReply(target.name + ' has not challenged you to a game of Tic-Tac-Toe.');
 		tttplayers[user.userid] = tttplayers[target.userid];
@@ -221,13 +223,13 @@ var cmds = {
 	dec: 'decline',
 	decline: function (target, room, user, connection, cmd) {
 		if (!target || !target.trim()) return this.sendReply('|html|/ttt ' + cmd + ' <em>User</em> - Declines a Tic-Tac-Toe challenge from a user.');
-		var targetUser = (Users.get(target) ? Users.get(target).name : target);
+		let targetUser = (Users.get(target) ? Users.get(target).name : target);
 		target = Users.get(target);
 		if (!target || !target.connected) return this.sendReply('User ' + targetUser + ' is offline.');
 		if (user.userid === target.userid) return this.sendReply('You can\'t use this command on yourself.');
-		var game = tttgames[tttplayers[toId(targetUser)]];
+		let game = tttgames[tttplayers[toId(targetUser)]];
 		if (!(target.userid in tttplayers) || !game.checkPlayer(target)) return this.sendReply(target + ' has not challenged you to a game of Tic-Tac-Toe.');
-		if (game.checkPlayer(target) && game.phase == 'started') return this.sendReply('You are playing with ' + game.checkPlayer(user) + ' right now. If you want to end the game, use /ttt end.');
+		if (game.checkPlayer(target) && game.phase === 'started') return this.sendReply('You are playing with ' + game.checkPlayer(user) + ' right now. If you want to end the game, use /ttt end.');
 
 		if (Users.get(target) && Users.get(target).connected) Users.get(target).send('|pm|' + user.getIdentity() + '|' + Users.get(target).getIdentity() + '|/error Your Tic-Tac-Toe request was declined.');
 		user.send('|pm|' + Users.get(target) + '|' + user.getIdentity() + '|/error You have declined the game request.');
@@ -238,7 +240,7 @@ var cmds = {
 	mark: 'markbox',
 	markbox: function (target, room, user, connection, cmd) {
 		if (!(user.userid in tttplayers)) return this.sendReply('You aren\'t playing a game of Tic-Tac-Toe right now.');
-		var game = tttgames[tttplayers[user.userid]];
+		let game = tttgames[tttplayers[user.userid]];
 		if (game.phase === 'waiting') return this.sendReply('The request has not been accepted yet. You can only use this command in an active game.');
 		game.markBox(user, target);
 	},
@@ -248,7 +250,7 @@ var cmds = {
 	show: 'see',
 	see: function (target, room, user) {
 		if (!(user.userid in tttplayers)) return this.sendReply('You aren\'t playing a game of Tic-Tac-Toe right now.');
-		var game = tttgames[tttplayers[user.userid]];
+		let game = tttgames[tttplayers[user.userid]];
 		if (game.phase === 'waiting') return this.sendReply('Your request has not been accepted yet. You can only use this command in an active game.');
 		game.update();
 	},
@@ -257,10 +259,10 @@ var cmds = {
 	leave: 'end',
 	end: function (target, room, user) {
 		if (!(user.userid in tttplayers)) return this.sendReply('You aren\'t playing a game of Tic-Tac-Toe right now.');
-		var game = tttgames[tttplayers[user.userid]];
+		let game = tttgames[tttplayers[user.userid]];
 		if (game.phase === 'waiting') game.end('The request was withdrawn.');
 		else game.end(user.name + ' has decided to leave the game midway.');
-	}
+	},
 };
 
 exports.commands = {
@@ -276,7 +278,7 @@ exports.commands = {
 	msg: function (target, room, user, connection) {
 		if (!target) return this.parse('/help msg');
 		target = this.splitTarget(target);
-		var targetUser = this.targetUser;
+		let targetUser = this.targetUser;
 		if (!target) {
 			this.sendReply("You forgot the comma.");
 			return this.parse('/help msg');
@@ -294,9 +296,9 @@ exports.commands = {
 		}
 
 		if (Config.pmmodchat) {
-			var userGroup = user.group;
+			let userGroup = user.group;
 			if (Config.groupsranking.indexOf(userGroup) < Config.groupsranking.indexOf(Config.pmmodchat)) {
-				var groupName = Config.groups[Config.pmmodchat].name || Config.pmmodchat;
+				let groupName = Config.groups[Config.pmmodchat].name || Config.pmmodchat;
 				this.errorReply("Because moderated chat is set, you must be of rank " + groupName + " or higher to PM users.");
 				return false;
 			}
@@ -324,9 +326,9 @@ exports.commands = {
 
 		if (target.charAt(0) === '/' && target.charAt(1) !== '/') {
 			// PM command
-			var innerCmdIndex = target.indexOf(' ');
-			var innerCmd = (innerCmdIndex >= 0 ? target.slice(1, innerCmdIndex) : target.slice(1));
-			var innerTarget = (innerCmdIndex >= 0 ? target.slice(innerCmdIndex + 1) : '');
+			let innerCmdIndex = target.indexOf(' ');
+			let innerCmd = (innerCmdIndex >= 0 ? target.slice(1, innerCmdIndex) : target.slice(1));
+			let innerTarget = (innerCmdIndex >= 0 ? target.slice(innerCmdIndex + 1) : '');
 			switch (innerCmd) {
 			case 'me':
 			case 'mee':
@@ -338,7 +340,7 @@ exports.commands = {
 				break;
 			case 'invite':
 			case 'inv':
-				var targetRoom = Rooms.search(innerTarget);
+				let targetRoom = Rooms.search(innerTarget);
 				if (!targetRoom || targetRoom === Rooms.global) return this.errorReply('The room "' + innerTarget + '" does not exist.');
 				if (targetRoom.staffRoom && !targetUser.isStaff) return this.errorReply('User "' + this.targetUsername + '" requires global auth to join room "' + targetRoom.id + '".');
 				if (targetRoom.isPrivate === true && targetRoom.modjoin && targetRoom.auth) {
@@ -362,10 +364,10 @@ exports.commands = {
 			}
 		}
 
-		var message = '|pm|' + user.getIdentity() + '|' + targetUser.getIdentity() + '|' + target;
+		let message = '|pm|' + user.getIdentity() + '|' + targetUser.getIdentity() + '|' + target;
 		user.send(message);
 		if (targetUser !== user) targetUser.send(message);
 		targetUser.lastPM = user.userid;
 		user.lastPM = targetUser.userid;
-	}
-}
+	},
+};

5edb70d9c7f8c58bdd219c84f6a76a3056fea17b
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-10 11:58:27 -0500

    Travis
    Additional errors spotted

--- mods/ashspokemon/scripts.js
+++ mods/ashspokemon/scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.BattleScripts = {
-	randomSeasonalMeleeTeam: function(side) {
+	randomSeasonalMeleeTeam: function (side) {
 		let team = [];
 		let sets = {
 			'Pikachu': {
@@ -447,7 +447,7 @@ exports.BattleScripts = {
 					spe: 252,
 					hp: 4,
 				},
-				nature: 'Jolly'
+				nature: 'Jolly',
 			},
 			'Torkoal': {
 				species: 'Torkoal',
@@ -651,25 +651,27 @@ exports.BattleScripts = {
 				};
 			} else {
 				for (let iv in {
-						hp: 31,
-						atk: 31,
-						def: 31,
-						spa: 31,
-						spd: 31,
-						spe: 31,
-					}) {
+					hp: 31,
+					atk: 31,
+					def: 31,
+					spa: 31,
+					spd: 31,
+					spe: 31,
+				}) {
 					set.ivs[iv] = iv in set.ivs ? set.ivs[iv] : 31;
 				}
 			}
 			// Assuming the hardcoded set evs are all legal.
-			if (!set.evs) set.evs = {
-				hp: 84,
-				atk: 84,
-				def: 84,
-				spa: 84,
-				spd: 84,
-				spe: 84,
-			};
+			if (!set.evs) {
+				set.evs = {
+					hp: 84,
+					atk: 84,
+					def: 84,
+					spa: 84,
+					spd: 84,
+					spe: 84,
+				};
+			}
 			set.moves = [this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves), this.sampleNoReplace(set.moves)].concat(set.signatureMove);
 			team.push(set);
 		}

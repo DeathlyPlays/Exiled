6cfe1fcd11985b9c25e4b2ed39884bdf2c1abe6e
Stellation <notquitesixty@gmail.com>
2017-04-10 21:42:59 -0400

    added exclamation marks

--- mods/essb/abilities.js
+++ mods/essb/abilities.js
@@ -556,18 +556,18 @@ exports.BattleAbilities = {
 		onTryHitPriority: 1,
 		onTryHit: function (pokemon, target, move) {
 			if (move.ohko) {
-				this.add('-immune', pokemon, '[msg]', '[from] ability: I\'M NOT HACKING');
+				this.add('-immune', pokemon, '[msg]', '[from] ability: I\'M NOT HACKING!!!');
 				return null;
 			}
 			if (move.flags['powder'] && target !== source && this.getImmunity('powder', target)) {
-				this.add('-immune', target, '[msg]', '[from] ability: I\'M NOT HACKING');
+				this.add('-immune', target, '[msg]', '[from] ability: I\'M NOT HACKING!!!');
 				return null;
 			}
 		},
 		onDamagePriority: -100,
 		onDamage: function (damage, target, source, effect) {
 			if (target.hp === target.maxhp && damage >= target.hp && effect && effect.effectType === 'Move') {
-				this.add('-ability', target, 'I\'M NOT HACKING');
+				this.add('-ability', target, 'I\'M NOT HACKING!!!');
 				return target.hp - 1;
 			}
 		},

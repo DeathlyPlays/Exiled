53d7ec6521006fd20a2826bbbdecc419ddf67ac7
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-11 16:33:22 -0500

    Travis

--- mods/mewth/pokedex.js
+++ mods/mewth/pokedex.js
@@ -1,76 +1,76 @@
 'use strict';
 
 exports.BattlePokedex = {
-    pikachubelle: {
-        num: 25,
-        species: "Pikachu-Belle",
-        baseSpecies: "Pikachu",
-        forme: "Belle",
-        formeLetter: "B",
-        types: ["Electric", "Psychic"],
-        gender: "F",
-        baseStats: {
-            hp: 35,
-            atk: 55,
-            def: 40,
-            spa: 50,
-            spd: 50,
-            spe: 90
-        },
-        abilities: {
-            0: "Lightning Rod"
-        },
-        heightm: 0.4,
-        weightkg: 6,
-        color: "Yellow",
-        eggGroups: ["Undiscovered"],
-    },
-    pikachupopstar: {
-        num: 25,
-        species: "Pikachu-Pop-Star",
-        baseSpecies: "Pikachu",
-        forme: "Pop-Star",
-        formeLetter: "P",
-        types: ["Electric", "Fairy"],
-        gender: "F",
-        baseStats: {
-            hp: 35,
-            atk: 55,
-            def: 40,
-            spa: 50,
-            spd: 50,
-            spe: 90
-        },
-        abilities: {
-            0: "Lightning Rod"
-        },
-        heightm: 0.4,
-        weightkg: 6,
-        color: "Yellow",
-        eggGroups: ["Undiscovered"],
-    },
-    pikachulibre: {
-        num: 25,
-        species: "Pikachu-Libre",
-        baseSpecies: "Pikachu",
-        forme: "Libre",
-        formeLetter: "L",
-        types: ["Electric", "Grass"],
-        gender: "F",
-        baseStats: {
-            hp: 35,
-            atk: 55,
-            def: 40,
-            spa: 50,
-            spd: 50,
-            spe: 90
-        },
-        abilities: {
-            0: "Lightning Rod"
-        },
-        heightm: 0.4,
-        weightkg: 6,
-        color: "Yellow",
-        eggGroups: ["Undiscovered"],
-    },
+	pikachubelle: {
+		num: 25,
+		species: "Pikachu-Belle",
+		baseSpecies: "Pikachu",
+		forme: "Belle",
+		formeLetter: "B",
+		types: ["Electric", "Psychic"],
+		gender: "F",
+		baseStats: {
+			hp: 35,
+			atk: 55,
+			def: 40,
+			spa: 50,
+			spd: 50,
+			spe: 90,
+		},
+		abilities: {
+			0: "Lightning Rod",
+		},
+		heightm: 0.4,
+		weightkg: 6,
+		color: "Yellow",
+		eggGroups: ["Undiscovered"],
+	},
+	pikachupopstar: {
+		num: 25,
+		species: "Pikachu-Pop-Star",
+		baseSpecies: "Pikachu",
+		forme: "Pop-Star",
+		formeLetter: "P",
+		types: ["Electric", "Fairy"],
+		gender: "F",
+		baseStats: {
+			hp: 35,
+			atk: 55,
+			def: 40,
+			spa: 50,
+			spd: 50,
+			spe: 90,
+		},
+		abilities: {
+			0: "Lightning Rod",
+		},
+		heightm: 0.4,
+		weightkg: 6,
+		color: "Yellow",
+		eggGroups: ["Undiscovered"],
+	},
+	pikachulibre: {
+		num: 25,
+		species: "Pikachu-Libre",
+		baseSpecies: "Pikachu",
+		forme: "Libre",
+		formeLetter: "L",
+		types: ["Electric", "Grass"],
+		gender: "F",
+		baseStats: {
+			hp: 35,
+			atk: 55,
+			def: 40,
+			spa: 50,
+			spd: 50,
+			spe: 90,
+		},
+		abilities: {
+			0: "Lightning Rod",
+		},
+		heightm: 0.4,
+		weightkg: 6,
+		color: "Yellow",
+		eggGroups: ["Undiscovered"],
+	},
 };

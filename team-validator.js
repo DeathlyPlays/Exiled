ec75535c8d6ba5ae6e59ff3d383696db59a9ca69
Insist <DeathlyPlays@users.noreply.github.com>
2017-04-20 13:21:28 -0500

    Update to main

--- team-validator.js
+++ team-validator.js
@@ -370,7 +370,6 @@ class Validator {
 					set.ivs = {hp: 30, atk: 30, def: 30, spa: 30, spd: 30, spe: 30};
 					for (let i in HPdvs) {
 						set.ivs[i] = HPdvs[i] * 2;
-
 					}
 				} else if (!canBottleCap) {
 					set.ivs = Validator.fillStats(tools.getType(set.hpType).HPivs, 31);

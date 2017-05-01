03410a93dce2718a65605389451949ba3a9636c5
Insist <DeathlyPlays@users.noreply.github.com>
2017-05-01 11:32:57 -0500

    Fix error

--- chat-plugins/exiled.js
+++ chat-plugins/exiled.js
@@ -42,7 +42,7 @@ Exiled.giveDailyReward = function (userid, user) {
 	let alts = Object.keys(user.prevNames).map(a => {return toId(a);});
 	let longestWait = 0;
 	for (let i = 0; i < alts.length; i++) {
-		let cur = Db.DailyBonus.get(alts[i])
+		let cur = Db('DailyBonus').get(alts[i]);
 		if (!cur) {
 			alts.splice(i, 1);
 			i--;
@@ -60,13 +60,12 @@ Exiled.giveDailyReward = function (userid, user) {
 	}
 	for (let i = 0; i < alts.length; i++) {
 		if ((Date.now() - lastTime) >= 127800000) Db('DailyBonus').set(alts[i], [1, Date.now()]);
-		if (Db('DailyBonus').get(alts[i])[0] === 8) Db('DailyBonus').set(alts[i], [7, Date.now()]);
-		Db('DailyBonus').set(userid, [(Db('DailyBonus').get(alts[i])[0] + 1), Date.now()]);
+		if (Db('DailyBonus').get(alts[i])[0] <= 8) Db('DailyBonus').set(alts[i], [7, Date.now()]);
 	}
 	let reward = Db('DailyBonus').get(userid)[0];
-	Economy.writeMoney(userid, Db('DailyBonus').get(userid)[0]);
-	user.send('|popup||wide||html| <center><u><b><font size="3">Exiled Daily Bonus</font></b></u><br>You have been awarded ' + Db('DailyBonus').get(userid)[0] + ' Buck.<br>' + showDailyRewardAni(userid) + '<br>Because you have connected to the server for the past ' + Db('DailyBonus').get(userid)[0] + ' Days.</center>');
-	Db('DailyBonus').set(userid, [(Db('DailyBonus').get(userid)[0] + 1), Date.now()]);
+	Economy.writeMoney(userid, reward);
+	for (let i = 0; i < alts.length; i++) Db('DailyBonus').set(alts[i], [(Db('DailyBonus').get(alts[i])[0] + 1), Date.now()]);
+	user.send('|popup||wide||html| <center><u><b><font size="3">SpacialGaze Daily Bonus</font></b></u><br>You have been awarded ' + reward + ' Bucks.<br>' + showDailyRewardAni(reward) + '<br>Because you have connected to the server for the past ' + reward + ' Days.</center>');
 };
 
 function showDailyRewardAni(streak) {

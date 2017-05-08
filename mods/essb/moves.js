79c63566f557cc939aa4edd10b429a6dbcba0b2c
Insist <DeathlyPlays@users.noreply.github.com>
2017-05-08 09:55:58 -0700

    Change back

--- mods/essb/moves.js
+++ mods/essb/moves.js
@@ -1367,7 +1367,7 @@ exports.BattleMovedex = {
 			this.attrLastMove('[still]');
 			this.add('-anim', source, "Toxic", target);
 		},
-		target: "foeSide",
+		target: "normal",
 		type: "Rock",
 	},
 	"punchyfury": {

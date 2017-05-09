0bc7e1060221ab8ba6696b044d0bee1ff66d5d9f
returningavenger <miguelmayers2001@gmail.com>
2017-05-09 23:56:52 +0000

    Digimon and PMD Edits

---
+++ mods/pmd/pokedex.js
@@ -0,0 +1,16 @@
+'use strict';
+
+exports.BattlePokedex = {
+	bittercold: {
+		num: -339,
+		species: "Cryogonal",
+		types: ["Ice"],
+		genderRatio: "N",
+		baseStats: {hp: 115, atk: 45, def: 100, spa: 45, spd: 100, spe: 20},
+		abilities: {0: "Color Change"},
+		heightm: 73.5,
+		weightkg: 246.5,
+		color: "Blue",
+		eggGroups: ["Undiscovered"],
+	},
+};

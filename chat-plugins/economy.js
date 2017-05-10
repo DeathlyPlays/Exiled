90f9f0ea8971bc62d954cbe68e28ae5b1d8a73f5
returningavenger <miguelmayers2001@gmail.com>
2017-05-10 12:35:18 +0000

    Economy Updates

--- chat-plugins/economy.js
+++ chat-plugins/economy.js
@@ -10,22 +10,6 @@ global.moneyName = 'Buck';
 global.moneyPlural = 'Bucks';
 
 /**
- * Gets an amount and returns the amount with the name of the money.
- *
- * @examples
- * moneyName(0); // 0 bucks
- * moneyName(1); // 1 buck
- * moneyName(5); // 5 bucks
- *
- * @param {Number} amount
- * @returns {String}
- */
-function moneyName(amount) {
-	let name = " buck";
-	return amount === 1 ? name : name + "s";
-}
-
-/**
  * Checks if the money input is actually money.
  *
  * @param {String} money
@@ -173,12 +157,12 @@ function handleBoughtItem(item, user, cost) {
 		Db('hasteam').set(user);
 		this.sendReply('You can now set your team!');
 	} else {
-		let msg = '**' + user.name + " has bought " + item + ".**";
-		Rooms.rooms.get("staff").add('|c|~Exiled Server|' + msg);
+		let mExiled = '**' + user.name + " has bought " + item + ".**";
+		Rooms.rooms.get("staff").add('|c|~Exiled Server|' + mExiled);
 		Rooms.rooms.get("staff").update();
 		Users.users.forEach(function (user) {
 			if (user.group === '~' || user.group === '&' || user.group === '@') {
-				user.send('|pm|~Exiled Server|' + user.getIdentity() + '|' + msg);
+				user.send('|pm|~Exiled Server|' + user.getIdentity() + '|' + mExiled);
 			}
 		});
 	}
@@ -200,77 +184,122 @@ exports.commands = {
 		});
 	},
 
-	givebuck: 'givemoney',
+	gb: 'givemoney', //You can change "gb" and "givebucks" to your money name for an alias that applies to your money Example: AwesomeBucks could be "ga" and "giveawesomebucks"
 	givebucks: 'givemoney',
-	givemoney: function (target, room, user) {
+	gm:'givemoney',
+	givemoney: function (target, room, user, connection, cmd) {
 		if (!this.can('forcewin')) return false;
-		if (!target || target.indexOf(',') < 0) return this.parse('/help givemoney');
+		if (!target) return this.sendReply("Usage: /" + cmd + " [user], [amount]");
+		let splitTarget = target.split(',');
+		if (!splitTarget[2]) return this.sendReply("Usage: /" + cmd + " [user], [amount], [reason]");
+		for (let u in splitTarget) splitTarget[u] = splitTarget[u].trim();
 
-		let parts = target.split(',');
-		let username = parts[0];
-		let amount = isMoney(parts[1]);
+		let targetUser = splitTarget[0];
+		if (toId(targetUser).length < 1) return this.sendReply("/" + cmd + " - [user] may not be blank.");
+		if (toId(targetUser).length > 19) return this.sendReply("/" + cmd + " - [user] can't be longer than 19 characters");
 
-		if (typeof amount === 'string') return this.errorReply(amount);
+		let amount = Math.round(Number(splitTarget[1]));
+		if (isNaN(amount)) return this.sendReply("/" + cmd + "- [amount] must be a number.");
+		if (amount > 1000) return this.sendReply("/" + cmd + " - You can't give more than 1000 " + moneyName + " at a time.");
+		if (amount < 1) return this.sendReply("/" + cmd + " - You can't give less than one " + moneyName + ".");
 
-		let total = Db('money').set(toId(username), Db('money').get(toId(username), 0) + amount).get(toId(username));
-		amount = amount + moneyName(amount);
-		total = total + moneyName(total);
-		this.sendReply(username + " was given " + amount + ". " + username + " now has " + total + ".");
-		if (Users.get(username)) Users(username).popup(user.name + " has given you " + amount + ". You now have " + total + ".");
-		Economy.logTransaction(username + " was given " + amount + " by " + user.name + ". " + username + " now has " + total);
+		let reason = splitTarget[2];
+		if (reason.length > 100) return this.errorReply("Reason may not be longer than 100 characters.");
+		if (toId(reason).length < 1) return this.errorReply("Please specify a reason to give " + moneyName + ".");
+
+		Economy.writeMoney(targetUser, amount, () => {
+			Economy.readMoney(targetUser, newAmount => {
+				if (Users(targetUser) && Users(targetUser).connected) {
+					Users.get(targetUser).popup('|html|You have received ' + amount + ' ' + (amount === 1 ? moneyName : moneyPlural) +
+					' from ' + Exiled.nameColor(user.userid, true) + '.');
+				}
+				this.sendReply(targetUser + " has received " + amount + ((amount === 1) ? " " + moneyName + "." : " " + moneyPlural + "."));
+				Economy.logTransaction(user.name + " has given " + amount + ((amount === 1) ? " " + moneyName + " " : " " + moneyPlural + " ") + " to " + targetUser + ". (Reason: " + reason + ") They now have " + newAmount + (newAmount === 1 ? " " + moneyName + "." : " " + moneyPlural + "."));
+			});
+		});
 	},
-	givemoneyhelp: ["/givemoney [user], [amount] - Give a user a certain amount of money."],
 
-	takebuck: 'takemoney',
+	tb: 'takemoney', //You can change "tb" and "takebucks" to your money name for an alias that applies to your money Example: AwesomeBucks could be "ta" and "takeawesomebucks"
 	takebucks: 'takemoney',
-	takemoney: function (target, room, user) {
+	tm:'takemoney',
+	takemoney: function (target, room, user, connection, cmd) {
 		if (!this.can('forcewin')) return false;
-		if (!target || target.indexOf(',') < 0) return this.parse('/help takemoney');
+		if (!target) return this.sendReply("Usage: /" + cmd + " [user], [amount]");
+		let splitTarget = target.split(',');
+		if (!splitTarget[2]) return this.sendReply("Usage: /" + cmd + " [user], [amount], [reason]");
+		for (let u in splitTarget) splitTarget[u] = splitTarget[u].trim();
 
-		let parts = target.split(',');
-		let username = parts[0];
-		let amount = isMoney(parts[1]);
+		let targetUser = splitTarget[0];
+		if (toId(targetUser) === user.userid) return this.errorReply("You cannot transfer bucks to yourself.");
+		if (toId(targetUser).length < 1) return this.sendReply("/" + cmd + " - [user] may not be blank.");
+		if (toId(targetUser).length > 19) return this.sendReply("/" + cmd + " - [user] can't be longer than 19 characters");
 
-		if (typeof amount === 'string') return this.errorReply(amount);
+		let amount = Math.round(Number(splitTarget[1]));
+		if (isNaN(amount)) return this.sendReply("/" + cmd + "- [amount] must be a number.");
+		if (amount > 1000) return this.sendReply("/" + cmd + " - You can't take more than 1000 " + moneyName + " at a time.");
+		if (amount < 1) return this.sendReply("/" + cmd + " - You can't take less than one " + moneyName + ".");
 
-		let total = Db('money').set(toId(username), Db('money').get(toId(username), 0) - amount).get(toId(username));
-		amount = amount + moneyName(amount);
-		total = total + moneyName(total);
-		this.sendReply(username + " lost " + amount + ". " + username + " now has " + total + ".");
-		if (Users.get(username)) Users(username).popup(user.name + " has taken " + amount + " from you. You now have " + total + ".");
-		Economy.logTransaction(username + " had " + amount + " taken away by " + user.name + ". " + username + " now has " + total);
+		let reason = splitTarget[2];
+		if (reason.length > 100) return this.errorReply("Reason may not be longer than 100 characters.");
+		if (toId(reason).length < 1) return this.errorReply("Please specify a reason to give " + moneyName + ".");
+
+		Economy.writeMoney(targetUser, -amount, () => {
+			Economy.readMoney(targetUser, newAmount => {
+				if (Users(targetUser) && Users(targetUser).connected) {
+					Users.get(targetUser).popup('|html|' + Exiled.nameColor(user.userid, true) + ' has removed ' + amount + ' ' + (amount === 1 ? moneyName : moneyPlural) +
+					' from you.<br />');
+				}
+				this.sendReply("You removed " + amount + ((amount === 1) ? " " + moneyName + " " : " " + moneyPlural + " ") + " from " + Chat.escapeHTML(targetUser));
+				Economy.logTransaction(user.name + " has taken " + amount + ((amount === 1) ? " " + moneyName + " " : " " + moneyPlural + " ") + " from " + targetUser + ". (Reason: " + reason + ") They now have " + newAmount + (newAmount === 1 ? " " + moneyName + "." : " " + moneyPlural + "."));
+			});
+		});
 	},
-	takemoneyhelp: ["/takemoney [user], [amount] - Take a certain amount of money from a user."],
 
-	transfer: 'transfermoney',
-	transferbuck: 'transfermoney',
+	confirmtransferbucks: 'transfermoney', //You can change "transferbucks" and "confirmtransferbucks" to your money name for an alias that applies to your money Example: AwesomeBucks could be "transferawesomebucks" and "confirmtransferawesomebucks"
 	transferbucks: 'transfermoney',
-	transfermoney: function (target, room, user) {
-		if (!target || target.indexOf(',') < 0) return this.parse('/help transfermoney');
+	confirmtransfermoney: 'transfermoney',
+	transfermoney: function (target, room, user, connection, cmd) {
+		if (!target) return this.sendReply("Usage: /" + cmd + " [user], [amount]");
+		let splitTarget = target.split(',');
+		for (let u in splitTarget) splitTarget[u] = splitTarget[u].trim();
+		if (!splitTarget[1]) return this.sendReply("Usage: /" + cmd + " [user], [amount]");
 
-		let parts = target.split(',');
-		let username = parts[0];
-		let uid = toId(username);
-		let amount = isMoney(parts[1]);
+		let targetUser = (Users.getExact(splitTarget[0]) ? Users.getExact(splitTarget[0]).name : splitTarget[0]);
+		if (toId(targetUser).length < 1) return this.sendReply("/" + cmd + " - [user] may not be blank.");
+		if (toId(targetUser).length > 18) return this.sendReply("/" + cmd + " - [user] can't be longer than 18 characters.");
 
-		if (toId(username) === user.userid) return this.errorReply("You cannot transfer to yourself.");
-		if (username.length > 19) return this.errorReply("Username cannot be longer than 19 characters.");
-		if (typeof amount === 'string') return this.errorReply(amount);
-		if (amount > Db('money').get(user.userid, 0)) return this.errorReply("You cannot transfer more money than what you have.");
-
-		Db('money')
-			.set(user.userid, Db('money').get(user.userid) - amount)
-			.set(uid, Db('money').get(uid, 0) + amount);
-
-		let userTotal = Db('money').get(user.userid) + moneyName(Db('money').get(user.userid));
-		let targetTotal = Db('money').get(uid) + moneyName(Db('money').get(uid));
-		amount = amount + moneyName(amount);
-
-		this.sendReply("You have successfully transferred " + amount + ". You now have " + userTotal + ".");
-		if (Users.get(username)) Users(username).popup(user.name + " has transferred " + amount + ". You now have " + targetTotal + ".");
-		Economy.logTransaction(user.name + " transferred " + amount + " to " + username + ". " + user.name + " now has " + userTotal + " and " + username + " now has " + targetTotal + ".");
+		let amount = Math.round(Number(splitTarget[1]));
+		if (isNaN(amount)) return this.sendReply("/" + cmd + " - [amount] must be a number.");
+		if (amount > 1000) return this.sendReply("/" + cmd + " - You can't transfer more than 1000 " + moneyName + " at a time.");
+		if (amount < 1) return this.sendReply("/" + cmd + " - You can't transfer less than one " + moneyName + ".");
+		Economy.readMoney(user.userid, money => {
+			if (money < amount) return this.sendReply("/" + cmd + " - You can't transfer more " + moneyName + " than you have.");
+			if (cmd !== 'confirmtransfermoney' && cmd !== 'confirmtransferbucks') {
+				return this.popupReply('|html|<center>' +
+					'<button class = "card-td button" name = "send" value = "/confirmtransfermoney ' + toId(targetUser) + ', ' + amount + '"' +
+					'style = "outline: none; width: 200px; font-size: 11pt; padding: 10px; border-radius: 14px ; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.4); box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.4) inset; transition: all 0.2s;">' +
+					'Confirm transfer to <br><b style = "color:' + Exiled.hashColor(targetUser) + '; text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.8)">' + Chat.escapeHTML(targetUser) + '</b></button></center>'
+				);
+			}
+			Economy.writeMoney(user.userid, -amount, () => {
+				Economy.writeMoney(targetUser, amount, () => {
+					Economy.readMoney(targetUser, firstAmount => {
+						Economy.readMoney(user.userid, secondAmount => {
+							this.popupReply("You sent " + amount + ((amount === 1) ? " " + moneyPlural : " " + moneyPlural) + " to " + targetUser);
+							Economy.logTransaction(
+								user.name + " has transfered " + amount + ((amount === 1) ? " " + moneyPlural : " " + moneyPlural) + " to " + targetUser + "\n" +
+								user.name + " now has " + secondAmount + " " + (secondAmount === 1 ? " " + moneyPlural : " " + moneyPlural) + " " +
+								targetUser + " now has " + firstAmount + " " + (firstAmount === 1 ? " " + moneyPlural : " " + moneyPlural)
+							);
+							if (Users.getExact(targetUser) && Users.getExact(targetUser).connected) {
+								Users.getExact(targetUser).send('|popup||html|' + Exiled.nameColor(user.name, true) + " has sent you " + amount + ((amount === 1) ? " " + moneyPlural : " " + moneyPlural));
+							}
+						});
+					});
+				});
+			});
+		});
 	},
-	transfermoneyhelp: ["/transfer [user], [amount] - Transfer a certain amount of money to a user."],
 
 	moneylog: function (target, room, user) {
 		if (!this.can('forcewin')) return false;
@@ -299,65 +328,30 @@ exports.commands = {
 		user.popup("|wide|" + output);
 	},
 
-	store: 'shop',
-	shop: function (target, room, user) {
-		if (!this.runBroadcast()) return;
-		return this.sendReply("|raw|" + shopDisplay);
-	},
-	shophelp: ["/shop - Display items you can buy with money."],
-
-	buy: function (target, room, user) {
-		if (!target) return this.parse('/help buy');
-		let amount = Db('money').get(user.userid, 0);
-		let cost = findItem.call(this, target, amount);
-		if (!cost) return;
-		let total = Db('money').set(user.userid, amount - cost).get(user.userid);
-		this.sendReply("You have bought " + target + " for " + cost + moneyName(cost) + ". You now have " + total + moneyName(total) + " left.");
-		room.addRaw(user.name + " has bought <b>" + target + "</b> from the shop.");
-		Economy.logTransaction(user.name + " has bought " + target + " from the shop. This user now has " + total + moneyName(total) + ".");
-		handleBoughtItem.call(this, target.toLowerCase(), user, cost);
-	},
-	buyhelp: ["/buy [command] - Buys an item from the shop."],
-
 	'!richestuser': true,
 	richestusers: 'richestuser',
 	richestuser: function (target, room, user) {
-		if (!target) target = 10;
+		if (!target) target = 100;
 		target = Number(target);
-		if (isNaN(target)) target = 10;
+		if (isNaN(target)) target = 100;
 		if (!this.runBroadcast()) return;
-		if (this.broadcasting && target > 10) target = 10; // limit to 10 while broadcasting
-		if (target > 500) target = 500;
-
-		let self = this;
-
-		function showResults(rows) {
-			let output = '<table border="1" cellspacing ="0" cellpadding="3"><tr><th>Rank</th><th>Name</th><th>' + moneyPlural + '</th></tr>';
-			let count = 1;
-			for (let u in rows) {
-				if (rows[u].amount < 1) continue;
-				output += '<tr><td>' + count + '</td><td>' + Exiled.nameColor(rows[u].name, true) + '</td><td>' + rows[u].amount + '</td></tr>';
-				count++;
-			}
-			self.sendReplyBox(output);
-			if (room) room.update();
-		}
-		let obj = Db('money').keys().map(function (name) {return {name: name, amount: Db('money').get(name)};});
-		let results = obj.sort(function (a, b) {
-			return b.amount - a.amount;
+		let keys = Db('money').keys().map(name => {
+			return {name: name, money: Db('money').get(name)};
 		});
-		showResults(results.slice(0, target));
+		if (!keys.length) return this.sendReplyBox("Money ladder is empty.");
+		keys.sort(function (a, b) { return b.money - a.money; });
+		this.sendReplyBox(rankLadder('Richest Users', moneyPlural, keys.slice(0, target), 'money') + '</div>');
 	},
 
-	resetbuck: 'resetmoney',
 	resetbucks: 'resetmoney',
 	resetmoney: function (target, room, user) {
-		if (!this.can('forcewin')) return false;
-		Db('money').set(toId(target), 0);
-		this.sendReply(target + " now has 0 bucks.");
-		Economy.logTransaction(user.name + " reset the money of " + target + ".");
+		if (!this.can('roomowner')) return false;
+		if (!target) return this.parse('/help resetmoney');
+		target = toId(target);
+		Economy.writeMoney(target, 0);
+		this.sendReply(target + " now has 0 " + moneyName + ".");
 	},
-	resetmoneyhelp: ["/resetmoney [user] - Reset user's money to zero."],
+	resetmoneyhelp: ['/resetmoney [user] - Resets target user\'s money to 0. Requires: &, ~'],
 
 	customsymbol: function (target, room, user) {
 		let bannedSymbols = ['!', '|', '‽', '\u2030', '\u534D', '\u5350', '\u223C'];
@@ -381,6 +375,7 @@ exports.commands = {
 		this.sendReply('Your symbol has been removed.');
 	},
 
+	money: 'economystats',
 	bucks: 'economystats',
 	economystats: function (target, room, user) {
 		if (!this.runBroadcast()) return;
@@ -388,9 +383,9 @@ exports.commands = {
 		const total = users.reduce(function (acc, cur) {
 			return acc + Db('money').get(cur);
 		}, 0);
-		let average = Math.floor(total / users.length) || '0';
-		let output = "There " + (total > 1 ? "are " : "is ") + total + moneyName(total) + " circulating in the economy. ";
-		output += "The average user has " + average + moneyName(average) + ".";
+		let average = Math.floor(total / users.length) || 0;
+		let output = "There " + (total > 1 ? "are " : "is ") + total + (total > 1 ? moneyPlural : moneyName) + " circulating in the economy. ";
+		output += "The average user has " + average + (average > 1 ? moneyPlural : moneyName) + ".";
 		this.sendReplyBox(output);
 	},
 };

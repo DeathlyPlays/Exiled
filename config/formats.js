45d9bce9246ddf4e0f7a0309fcb33315234a2dc0
VXN <notquitesixty@gmail.com>
2017-05-11 21:02:50 -0400

    Optimize type illusion ruleset (#148)
    * Update formats.js
    
    * Remove nonexistent mod

--- config/formats.js
+++ config/formats.js
@@ -1416,8 +1416,7 @@ exports.Formats = [
 	{
 		name: "[Gen 7] Type Illusion Beta",
 		section: "Exiled's Custom Gamemodes",
-		mod: 'gen7',
-		ruleset: ['Sleep Clause Mod', 'HP Percentage Mod', 'Cancel Mod', 'Pokemon', 'Standard'],
+		ruleset: ['[Gen 7] OU'],
 		desc: [
 			"&bullet; Coded by flufi.",
 			"Nickname a Pokemon a single typing and the Pokemon will change to that typing in battle.",

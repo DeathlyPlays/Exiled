628c99028e7383b7778d802355b985b9f721098a
returningavenger <miguelmayers2001@gmail.com>
2017-05-18 11:21:31 +0000

    Update to main

--- chat.js
+++ chat.js
@@ -193,6 +193,7 @@ class CommandContext {
 						this.user.sendTo(this.room, (this.room.type === 'chat' ? '|c:|' + (~~(Date.now() / 1000)) + '|' : '|c|') + this.user.getIdentity(this.room.id) + '|' + message);
 					} else {
 						this.room.add(`|c|${this.user.getIdentity(this.room.id)}|${message}`).update();
+						Exiled.addExp(this.user, this.room, 1);
 					}
 				}
 			}

2434fbaa8a85adfdb5a6712742504e72f0bb3c1a
DeathlyPlays <julies4porta@gmail.com>
2017-10-08 05:40:57 +0000

    DigiDex: Allow Broadcasting

--- server-plugins/server.js
+++ server-plugins/server.js
@@ -1133,8 +1133,13 @@ exports.commands = {
 	'!digidex': true,
 	dd: 'digidex',
 	digidex: function (target) {
+		if (!this.runBroadcast()) return;
 		if (!target) return this.parse("/help digidex");
-		this.parse("/dt " + target + ", digimon");
+		if (this.broadcasting) {
+			this.parse("!dt " + target + ", digimon");
+		} else {
+			this.parse("/dt " + target + ", digimon");
+		}
 	},
 	digidexhelp: ["/digidex [Digimon] - Checks for a Digimon's data from Digimon Showdown."],
 

dbb33cbf7710b6a2f9b568a02413bc7d937bc07c
DeathlyPlays <julies4porta@gmail.com>
2017-10-10 19:15:39 +0000

    Roomshop: Simplify

--- server-plugins/roomshop.js
+++ server-plugins/roomshop.js
@@ -8,7 +8,6 @@
 'use strict';
 
 const fs = require('fs');
-const db = require('origindb')('./config/db');
 const MAX_ITEMS = 12;
 
 exports.commands = {
@@ -17,63 +16,63 @@ exports.commands = {
 	leagueshop: {
 		enable: function (target, room, user) {
 			if (!this.can('eval')) return false;
-			if (db('roomshop').has(room.id)) return this.errorReply('Roomshop is already enabled here.');
+			if (Db('roomshop').has(room.id)) return this.errorReply('Roomshop is already enabled here.');
 
-			db('roomshop')
+			Db('roomshop')
 				.set(room.id, {});
 
 			this.sendReply('Roomshop has been enabled here.');
 		},
 		disable: function (target, room, user) {
 			if (!this.can('eval')) return false;
-			if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+			if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
 
-			db('roomshop').delete(room.id);
+			Db('roomshop').delete(room.id);
 
 			this.sendReply('Roomshop has been disabled here.');
 		},
 		bank: {
 			set: function (target, room, user) {
 				if (!this.can('declare', null, room)) return false;
-				if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
-				if (db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' already has a bank.');
+				if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+				if (Db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' already has a bank.');
 
 				let bank = toId(target);
 
 				if (!bank) return this.parse('/help roomshop');
 
-				db('roomshop')
+				Db('roomshop')
 					.set([room.id, 'Bank'], bank);
 
 				this.sendReply(bank + ' has been made the bank for ' + room.id + '\'s shop.');
 			},
 			change: function (target, room, user) {
 				if (!this.can('declare', null, room)) return false;
-				if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
-				if (!db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' does not have a bank set.');
+				if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+				if (!Db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' does not have a bank set.');
 
 				let bank = toId(target);
 
 				if (!bank) return this.parse('/help roomshop');
 
-				db('roomshop')
+				Db('roomshop')
 					.set([room.id, 'Bank'], bank);
 
 				this.sendReply(bank + ' has been made the bank for ' + room.id + '\'s shop.');
 			},
 			'': function (target, room, user) {
 				if (!this.can('declare', null, room)) return false;
-				if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
-				if (!db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' does not have a bank set.');
+				if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+				if (!Db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' does not have a bank set.');
 
-				let bank = db('roomshop').get([room.id, 'Bank']);
+				let bank = Db('roomshop').get([room.id, 'Bank']);
 
 				this.sendReply(bank + ' is currently the bank for the roomshop.');
 			},
 		},
 		add: function (target, room, user) {
 			if (!this.can('declare', null, room)) return false;
-			if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+			if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
 
 			let opts = target.split(',');
 
@@ -84,14 +83,14 @@ exports.commands = {
 
 			if (!itemID || !description || !price || itemID === 'bank') return this.parse('/help roomshop');
 			if (isNaN(price)) return this.errorReply('Price should be in integers.');
-			if (db('roomshop').has([room.id, itemID])) return this.errorReply(itemID + ' is already present in roomshop.');
+			if (Db('roomshop').has([room.id, itemID])) return this.errorReply(itemID + ' is already present in roomshop.');
 
-			let keys = Object.keys(db('roomshop').object()[room.id]);
+			let keys = Object.keys(Db('roomshop').object()[room.id]);
 
 			if (keys.indexOf('Bank') >= 0) keys.splice(keys.indexOf('Bank'), 1);
 			if (keys.length >= MAX_ITEMS) return this.errorReply('roomshop has reached max number of items.');
 
-			db('roomshop')
+			Db('roomshop')
 				.set([room.id, itemID], {})
 				.set([room.id, itemID, 'Item Name'], itemName)
 				.set([room.id, itemID, 'Description'], description)
@@ -101,36 +100,36 @@ exports.commands = {
 		},
 		remove: function (target, room, user) {
 			if (!this.can('declare', null, room)) return false;
-			if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+			if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
 
 			let itemID = toId(target);
 
 			if (!itemID) return this.parse('/help roomshop');
 
-			if (!db('roomshop').has([room.id, itemID]) || itemID === 'bank') return this.errorReply(itemID + ' is not present in roomshop.');
-			db('roomshop').delete([room.id, itemID]);
+			if (!Db('roomshop').has([room.id, itemID]) || itemID === 'bank') return this.errorReply(itemID + ' is not present in roomshop.');
+			Db('roomshop').delete([room.id, itemID]);
 
 			this.sendReply(itemID + ' has been removed from the roomshop.');
 		},
 		buy: function (target, room, user) {
-			if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+			if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
 
 			let itemID = toId(target);
 
-			if (!db('roomshop').has([room.id, itemID]) || itemID === 'bank') return this.errorReply(itemID + ' is not present in roomshop.');
-			if (!db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' does not have a bank set.');
-			if (db('roomshop').get([room.id, 'Bank']) === user.userid) return this.errorReply('Bank cannot purchase from the roomshop.');
+			if (!Db('roomshop').has([room.id, itemID]) || itemID === 'bank') return this.errorReply(itemID + ' is not present in roomshop.');
+			if (!Db('roomshop').has([room.id, 'Bank'])) return this.errorReply(room.id + ' does not have a bank set.');
+			if (Db('roomshop').get([room.id, 'Bank']) === user.userid) return this.errorReply('Bank cannot purchase from the roomshop.');
 
 			let usersMoney = Db('money').get(user.userid, 0);
-			let bank = db('roomshop').get([room.id, 'Bank']);
+			let bank = Db('roomshop').get([room.id, 'Bank']);
 			let banksMoney = Db('money').get(bank, 0);
-			let cost = db('roomshop').get([room.id, itemID, 'Price']);
+			let cost = Db('roomshop').get([room.id, itemID, 'Price']);
 
 			if (usersMoney < cost) return this.errorReply('You do not have enough money to purchase ' + itemID + '.');
 
 			Db('money').set(user.userid, usersMoney - cost).get(user.userid);
 			Db('money').set(bank, banksMoney + cost).get(bank);
-			db('roomshop').set();
+			Db('roomshop').set();
 
 			if (!fs.existsSync('logs/roomshops')) fs.mkdirSync('logs/roomshops');
 			fs.appendFile('logs/roomshops/roomshop_' + room.id + '.txt', '[' + new Date().toUTCString() + '] ' + user.name + ' has bought ' + itemID + ' from the roomshop.' + '\n');
@@ -148,9 +147,9 @@ exports.commands = {
 		},
 		'': function (target, room, user) {
 			if (!this.runBroadcast()) return;
-			if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+			if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
 
-			let keys = Object.keys(db('roomshop').object()[room.id]);
+			let keys = Object.keys(Db('roomshop').object()[room.id]);
 
 			function display(keys) {
 				let display = '<center><u><strong>' + room.title + '\'s Shop</strong></u></center><br><table border="1" cellspacing="0" cellpadding="5" width="100%"><tbody><tr><th>Item</th><th>Description</th><th>Price</th></tr>';
@@ -159,7 +158,7 @@ exports.commands = {
 				if (!keys.length) return '<center><h2><strong><u>This Shop Has No Items!</u></strong></h2></center>';
 
 				for (let i = 0; i < keys.length; i++) {
-					let item = db('roomshop').object()[room.id][keys[i]];
+					let item = Db('roomshop').object()[room.id][keys[i]];
 					display += '<tr><td align="center"><button name="send" value="/roomshop buy ' + keys[i] + '"><strong>' + item['Item Name'] + '</strong></button></td>';
 					display += '<td align="center">' + item['Description'] + '</td>';
 					display += '<td align="center">' + item['Price'] + '</td></tr>';
@@ -173,7 +172,7 @@ exports.commands = {
 		},
 		log: function (target, room, user) {
 			if (!this.can('declare', null, room)) return false;
-			if (!db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
+			if (!Db('roomshop').has(room.id)) return this.errorReply('Roomshop is not enabled here.');
 			target = toId(target);
 
 			let numLines = 15;

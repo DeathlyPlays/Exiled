5cf7ec2aeb2103cb4edf02a0669c5aea03442b7b
returningavenger <miguelmayers2001@gmail.com>
2018-01-11 19:42:01 +0000

    Icons: Default to /iconhelp

--- server-plugins/icon.js
+++ server-plugins/icon.js
@@ -52,11 +52,12 @@ exports.commands = {
 			if (icons[toId(target[0])]) return this.errorReply("This user already has a custom userlist icon.  Do /icon delete [user] and then set their new icon.");
 			this.sendReply("|raw|You have given " + Server.nameColor(target[0], true) + " an icon.");
 			Monitor.log(target[0] + " has received an icon from " + user.name + ".");
-			this.privateModAction("|raw|(" + target[0] + " has recieved icon: <img src='" + target[1] + "' width='32' height='32'> from " + user.name + ".)");
+			this.privateModAction("|raw|(" + target[0] + " has received icon: <img src='" + target[1] + "' width='32' height='32'> from " + user.name + ".)");
 			if (Users(target[0]) && Users(target[0]).connected) Users(target[0]).popup("|html|" + Server.nameColor(user.name, true) + " has set your userlist icon to: <img src='" + target[1] + "' width='32' height='32'><br><center>Refresh, If you don't see it.</center>");
 			icons[toId(target[0])] = target[1];
 			updateIcons();
 		},
+
 		remove: 'delete',
 		delete: function (target, room, user) {
 			if (!this.can('lock')) return false;
@@ -68,6 +69,11 @@ exports.commands = {
 			Monitor.log(user.name + " removed " + target + "'s icon.");
 			this.privateModAction("(" + target + "'s icon was removed by " + user.name + ".)");
 			if (Users(target) && Users(target).connected) Users(target).popup("|html|" + Server.nameColor(user.name, true) + " has removed your userlist icon.");
+		},
+
+		"": "help",
+		help: function (target, room, user) {
+			this.parse("/iconhelp");
 		},
 	},
 	iconhelp: [

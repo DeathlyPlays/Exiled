db73b4012339be7af8f1625a1d08ef950856af63
Cycloning Hurricane <dlucano@yahoo.com>
2018-01-22 16:57:21 +0000

    Update

--- server-plugins/server.js
+++ server-plugins/server.js
@@ -151,6 +151,25 @@ function saveRegdateCache() {
 	fs.writeFileSync('config/regdate.json', JSON.stringify(regdateCache));
 }
 
+let messages = [
+	"has vanished into nothingness!",
+	"used Explosion!",
+	"fell into the void.",
+	"went into a cave without a repel!",
+	"has left the building.",
+	"was forced to give StevoDuhHero's mom an oil massage!",
+	"was hit by Magikarp's Revenge!",
+	"ate a bomb!",
+	"is blasting off again!",
+	"(Quit: oh god how did this get here i am not good with computer)",
+	"was unfortunate and didn't get a cool message.",
+	"{{user}}'s mama accidently kicked {{user}} from the server!",
+	"felt Insist's wrath.",
+	"got rekt by Travis CI!",
+	"exited life.exe.",
+	"found a species called \"friends\" (whatever that means).",
+];
+
 exports.commands = {
 	useroftheweek: 'uotw',
 	uotw: function (target, room, user) {

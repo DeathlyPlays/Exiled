536246ee230174019fddd1696cb7ef4f60f9e769
returningavenger <miguelmayers2001@gmail.com>
2018-03-14 13:59:24 +0000

    Playlist: Rewrite using lib/fs.js/Replace cases

--- server-plugins/playlist.js
+++ server-plugins/playlist.js
@@ -1,99 +1,127 @@
-/*******************************
-*                              *
-*        Playlists             *
-*        By Execute            *
-*   Useful for sharing music.  *
-*                              *
-********************************/
+/************************************
+ * Playlists for Pokemon Showdown	*
+ * Created by Execute				*
+ * Rewritten by Insist				*
+ ************************************/
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
+let blackcss = "background: #3C3B42; padding: 10px; color: white; border: 1px solid black; border-radius: 6px; text-align: center;";
 
-let blackcss = 'background:#3C3B42;padding:10px;color:white;border:1px solid black; border-radius:6px;text-align:center;';
+let MAXIMUM_SONGS = 9;
 
-let data = {};
+const FS = require("../lib/fs.js");
 
-try {
-	data = JSON.parse(fs.readFileSync('config/playlist.json', 'utf8'));
-} catch (e) {
-	console.log('Error reading playlist data, resorting to resetting all data entries....');
-	fs.writeFile('config/playlist.json', JSON.stringify(data), () => {});
-	console.log('Playlist data erased.');
+let playlists = FS("config/playlists.json").readIfExistsSync();
+
+if (playlists !== "") {
+	playlists = JSON.parse(playlists);
+} else {
+	playlists = {};
 }
 
 function save() {
-	fs.writeFile('config/playlist.json', JSON.stringify(data), () => {});
+	FS("config/playlist.json").write(JSON.stringify(playlists));
 }
 
 function playlistGenerator(user) {
-	let playlistData = data[user];
-	if (!data[user] || data[user].length < 1) return Server.font('This user does not have any playlist data.', 'maroon', true);
-	let display = '<div style="' + blackcss + '">';
+	let playlistData = playlists[user];
+	if (!playlists[user] || playlists[user].length < 1) return Server.font(`This user does not have any playlists.`, `maroon`, true);
+	let display = `<div style="${blackcss}">`;
 	for (let i = 0; i < playlistData.length; i++) {
-		display += '<button name="send" value="/playlist play, ' + playlistData[i][0] + ', ' + user + '">' + playlistData[i][1] + '</button>';
+		display += `<button name="send" value="/playlist play ${playlistData[i][0]}, ${user}">${playlistData[i][1]}</button>`;
 	}
-	display += '</div>';
+	display += `</div>`;
 	return display;
 }
 
 exports.commands = {
-	playlist: function (target, room, user) {
-		if (!target) return this.sendReply('|uhtml|' + user.name + 'playlist|' + playlistGenerator(user.userid));
-		let parts = target.split(',');
-		switch (toId(parts[0])) {
-		case 'add':
-			if (parts.length < 3) return this.parse('/playlist help');
-			let link = parts[1].trim();
-			let title = parts[2].trim();
-			if (!data[user.userid]) data[user.userid] = [];
-			if (data[user.userid].length > 9) return this.errorReply('You already have exceeded the maximum amount of songs a user may have in their playlist.');
-			for (let i = 0; i < data[user.userid].length; i++) {
-				if (data[user.userid][i][0] === link) return this.errorReply('You already have this song in your playlist.');
+	musicbox: "playlist",
+	playlist: {
+		add: function (target, room, user) {
+			let parts = target.split(",");
+			if (parts.length < 2) return this.parse("/playlist help");
+			let link = parts[0].trim();
+			let title = parts[1].trim();
+			if (!playlists[user.userid]) playlists[user.userid] = [];
+			if (playlists[user.userid].length > MAXIMUM_SONGS) return this.errorReply("You already have exceeded the maximum amount of songs a user may have in their playlist.");
+			for (let i = 0; i < playlists[user.userid].length; i++) {
+				if (playlists[user.userid][i][0] === link) return this.errorReply("You already have this song in your playlist.");
 				continue;
 			}
-			data[user.userid].push([link, title]);
+			playlists[user.userid].push([link, title]);
 			save();
-			return this.sendReply('This song has been added to your playlist!');
-		case 'delete':
-			if (parts.length < 2) return this.parse('/playlist help');
-			let Title = parts[1].trim();
-			if (!data[user.userid]) data[user.userid] = [];
-			for (let i = 0; i < data[user.userid].length; i++) {
-				if (data[user.userid][i][1] === Title) {
-					data[user.userid].splice(data[user.userid].indexOf(data[user.userid][i]), 1);
+			return this.sendReply("This song has been added to your playlist!");
+		},
+
+		remove: "delete",
+		delete: function (target, room, user) {
+			let title = toId(target);
+			if (!title) return this.parse("/playlist help");
+			if (!playlists[user.userid]) playlists[user.userid] = [];
+			for (let i = 0; i < playlists[user.userid].length; i++) {
+				if (playlists[user.userid][i].titleId === title) {
+					playlists[user.userid].splice(playlists[user.userid].indexOf(playlists[user.userid][i]), 1);
 					save();
-					return this.sendReply('This song has been removed from your playlist.');
+					return this.sendReply("This song has been removed from your playlist.");
 				} else {
 					continue;
 				}
 			}
-			return this.errorReply('This song is not in your playlist');
-		case 'reset':
-			data[user.userid] = [];
-			save();
-			this.sendReply('You have reset your playlist.');
-			break;
-		case 'play':
-			if (parts.length < 3) return this.parse('/playlist help');
-			let userid = toId(parts[2]);
-			if (!data[userid]) data[userid] = [];
-			for (let i = 0; i < data[userid].length; i++) {
-				if (data[userid][i][0] === parts[1].trim()) return this.sendReply('|uhtmlchange|' + userid + 'playlist|<div style="' + blackcss + '"><audio src="' + data[userid][i][0] + '" controls>' + data[userid][i][1] + '</audio><div style="float:right;"><button name="send" value="/playlist back, ' + userid + '">Playlist</button></div></div>');
-			}
-			return this.errorReply('Your playlist does not contain this song.');
-		case 'back':
-			if (!parts[1]) return this.sendReply('|uhtmlchange|' + user.name + 'playlist|' + playlistGenerator(user.name));
-			return this.sendReply('|uhtmlchange|' + toId(parts[1]) + 'playlist|' + playlistGenerator(toId(parts[1])));
+			return this.errorReply(`This song is not in your playlist.`);
+		},
 
-		default:
-			return this.sendReply('|uhtml|' + toId(parts[0]) + 'playlist|' + playlistGenerator(toId(parts[0])));
-		}
+		nuke: "reset",
+		clearall: "reset",
+		reset: function (target, room, user) {
+			if (!target) {
+				playlists[user.userid] = [];
+				save();
+				return this.sendReply(`You have reset your playlist.`);
+			} else {
+				if (!this.can("lock") && toId(target) !== user.userid) return false;
+				playlists[toId(target)] = [];
+				save();
+				return this.sendReply(`You have reset ${target}'s playlist.`);
+			}
+		},
+
+		listen: "play",
+		play: function (target, room, user) {
+			let parts = target.split(",");
+			if (parts.length < 2) return this.parse("/playlist help");
+			let userid = toId(parts[1]);
+			if (!playlists[userid]) playlists[userid] = [];
+			for (let i = 0; i < playlists[userid].length; i++) {
+				if (playlists[userid][i][0] === parts[0].trim()) return this.sendReply(`|uhtmlchange|${userid}playlist|<div style="${blackcss}"><audio src="${playlists[userid][i][0]}" controls>${playlists[userid][i][1]}</audio><div style="float: right;"><button name="send" value="/playlist back ${userid}">Playlist</button></div></div>`);
+			}
+			return this.errorReply("Your playlist does not contain this song.");
+		},
+
+		back: function (target, room, user) {
+			if (!target) return this.sendReply(`|uhtmlchange|${user.userid}playlist|${playlistGenerator(user.userid)}`);
+			return this.sendReply(`|uhtmlchange|${toId(target)}playlist|${playlistGenerator(toId(target))}`);
+		},
+
+		"": "list",
+		playlist: "list",
+		list: function (target, room, user) {
+			if (!this.runBroadcast()) return;
+			if (!target) target = user.userid;
+			return this.sendReply(`|uhtml|${toId(target)}playlist|${playlistGenerator(toId(target))}`);
+		},
+
+		help: function () {
+			this.parse("/playlisthelp");
+		},
 	},
 	playlisthelp: [
-		"/playlist add, [song mp3 url], [title of the song] - Adds a song to your playlist [Limit of 9 songs].",
-		"/playlist delete, [title of the song] - Deletes a song from your playlist.",
-		"/playlist reset - Removes all songs from your playlist.",
-		"/playlist - Displays your playlist.",
+		`/playlist add [song mp3 url], [title of the song] - Adds a song to your playlist [Limit of ${MAXIMUM_SONGS} songs].
+		/playlist delete [title of the song] - Deletes a song from your playlist.
+		/playlist play [song link], [optional user] - Plays the song from the specified user's playlist; Defaults to yourself.
+		/playlist reset [optional target] - Removes all songs from your playlist. If you are a Global % or up ad include a target you can reset the target's playlist.
+		/playlist back [optional target] - Returns to the menu of the playlist; Defaults to yourself.
+		/playlist list [optional target] - Displays the target's playlist; Defaults to yourself.
+		/playlist help - Displays the help command.`,
 	],
 };

dab0cf26b9a67b4a5eb65ad82ffa909701d13a02
returningavenger <miguelmayers2001@gmail.com>
2018-05-05 05:04:58 +0000

    Sentence Game: Rewrite

--- server-plugins/sentencegame.js
+++ server-plugins/sentencegame.js
@@ -1,9 +1,12 @@
-'use strict';
+/****************************
+ * Sentence Games for PS!	*
+ * Inspired by fender & ASL	*
+ * Rewrote by Insist		*
+ ****************************/
 
-/************************************************
- * Sentence Game by fender and AuraStormLucario *
- ************************************************/
-const badEndings = ['the', 'and', 'a', 'an', 'or', 'with', 'to'];
+"use strict";
+
+const badEndings = ["the", "and", "a", "an", "or", "with", "to"];
 
 function cleanWord(word) {
 	let reg = /[^a-zA-Z0-9 :]/g;
@@ -15,67 +18,89 @@ function cleanWord(word) {
 	}
 }
 
-function endDisplay(arr) {
-	let result = arr.join(' ');
-	let display = '<div class="sentence-container"><font size="4">The Sentence has been finished!<br />"<i>' + result + '.</i>"</font></div>';
-	return display;
+class SentenceGame {
+	constructor(room, wordCount) {
+		this.room = room;
+		this.wordCount = wordCount;
+		this.sentence = [];
+		this.lastUser = null;
+		this.room.addRaw(`<div style="broadcast-green"><h2 style="font-weight: bold">A Sentence Game has been created with ${this.wordCount} words!</h2><small style="font-style: italic">You may add words by using /sentence aw [word].</small></div>`);
+	}
+
+	addWord(user, word, self) {
+		if (this.lastUser === user.userid) return self.errorReply(`You were the last user to add a word, try letting other users join in.`);
+		if (word === this.sentence[this.sentence.length - 1]) return self.errorReply(`${word} was the last word, try something else to make it interesting.`);
+		if (cleanWord(word) === false) return self.errorReply(`You may only use alpha characters.`);
+		if (word.length > 20) return self.errorReply(`Let's try to keep words a maximum of 20 characters.`);
+		if (word.indexOf(" ") > -1) return self.errorReply(`You may not add spaces (don't worry we automatically space for you).`);
+		if (this.sentence.length === this.wordCount - 1 && badEndings.indexOf(word) > -1) return self.errorReply(`${word} is a boring way to end this sentence.`);
+		this.sentence.push(word);
+		let wordsLeft = this.wordCount - this.sentence.length;
+		this.room.addRaw(`${Server.nameColor(user.name, true, true)} has added "${word}" to the sentence.<br />Sentence: ${this.sentence.join(" ")}.<br />${wordsLeft === 0 ? `` : `<small>There ${wordsLeft === 1 ? `is` : `are`} ${wordsLeft} word${wordsLeft === 1 ? `` : `s`} remaining in this Sentence Game.</small>`}`);
+		if (this.sentence.length === this.wordCount) {
+			this.endGame();
+		}
+	}
+
+	endGame(forced) {
+		if (!forced) this.room.addRaw(`<div class="broadcast-green"><strong style="text-align: center">This Sentence Game has been finished!<br /><br />Final Sentence: ${this.sentence.join(" ")}.</div>`);
+		if (forced) this.room.addRaw(`${Server.nameColor(forced, true, true)} has ended the Sentence Game in this room forcefully.`);
+		delete this.room.sentence;
+	}
 }
 
 exports.commands = {
-	newsentence: 'sentence',
-	sentence: function (target, room, user) {
-		if (!this.can('mute', null, room)) return this.errorReply("/sentence - Access denied.");
-		if (room.sentence) return this.errorReply("There is already a sentence game in this room.");
-		if (room.battle) return this.errorReply("You cannot play sentence game in battle rooms.");
-		if (!target) return this.errorReply("You must specify a sentence length.");
-		if (target === 'help') return this.parse('/help sentence');
+	sentencegame: "sentence",
+	sentence: {
+		create: "new",
+		make: "new",
+		new: function (target, room, user) {
+			if (!this.can("minigame", null, room)) return false;
+			if (!this.canTalk()) return;
+			if (room.sentence) return this.errorReply(`There is already a Sentence Game in this room.`);
+			if (!target) target = 10;
+			if (isNaN(target)) return this.errorReply(`The word count must be a valid number.`);
+			if (target < 5 || target > 20) return this.errorReply(`The word count must be at least 5 and a maximum of 20.`);
+			room.sentence = new SentenceGame(room, target);
+		},
 
-		let length = Math.floor(Number(target));
-		if (isNaN(length)) return this.errorReply("The length must be a number.");
-		if (length > 15 || length < 2) return this.errorReply("The sentence cannot be this long.");
+		addword: "aw",
+		aw: function (target, room, user) {
+			if (!this.canTalk()) return;
+			if (!room.sentence) return this.errorReply(`There isn't a Sentence Game in this room.`);
+			if (!target) return this.errorReply(`Please include the word you want to add.`);
+			room.sentence.addWord(user, target, this);
+		},
 
-		room.sentence = {};
-		room.sentence.size = length;
-		room.sentence.result = [];
-		room.sentence.lastUser = '';
-		room.addRaw('<div class="sentence-container"><font size="4">A Sentence Game has been started!<br />The sentence will be <font color="red">' + target + '</font> words long.</font></div>');
-	},
+		info: "info",
+		displaysentence: "show",
+		display: "show",
+		showsentence: "show",
+		show: function (target, room, user) {
+			if (!this.runBroadcast()) return;
+			if (!room.sentence) return this.errorReply(`There isn't a Sentence Game in this room.`);
+			if (room.sentence.sentence.length < 1) return this.errorReply(`The sentence hasn't begun in this game yet.`);
+			return this.sendReplyBox(`<strong>Current Sentence:</strong> ${room.sentence.sentence.join(" ")}.<br /><strong>Progress:</strong> ${room.sentence.sentence.length}/${room.sentence.wordCount} words.<br /><strong>Last User:</strong> ${Server.nameColor(room.sentence.lastUser, true, true)}.`);
+		},
 
-	aw: 'addword',
-	addword: function (target, room, user) {
-		if (!room.sentence) return this.errorReply("There is not a sentence game in this room.");
-		if (!target) return this.errorReply("You must specify a word to add.");
-		if (target.length > 16) return this.errorReply("Your word should not be longer than 16 characters.");
-		if (target.indexOf(" ") > -1) return this.errorReply("Your word should not include spaces.");
-		if (room.sentence.lastUser === user.userid) return this.errorReply("You just gave the previous word. Give someone else a turn!");
+		end: function (target, room, user) {
+			if (!this.can("minigame", null, room)) return false;
+			if (!this.canTalk()) return;
+			if (!room.sentence) return this.errorReply(`There is no Sentence Game to end in this room.`);
+			room.sentence.endGame(user.name);
+		},
 
-		let word = cleanWord(target);
-		if (word === false) return this.errorReply("The word must contain only alpha characters.");
-		if (room.sentence.result.length === room.sentence.size - 1 && badEndings.indexOf(word) > -1) return this.errorReply("Nobody wants the sentence to end with '" + word + "'.");
-		room.sentence.result.push(word);
-
-		room.addRaw('<div class="sentence-container"><font size="3">' + Server.nameColor(user.userid, true) + ' has added the word "<font color="red">' + word + '</font>" to the sentence.</font></div>');
-
-		if (room.sentence.result.length === room.sentence.size) {
-			let end = endDisplay(room.sentence.result);
-			room.addRaw(end);
-			delete room.sentence;
-			return;
-		} else {
-			room.sentence.lastUser = user.userid;
-		}
-	},
-
-	endsentence: function (target, room, user) {
-		if (!this.can('mute', null, room)) return this.errorReply("/endsentence - Access denied.");
-		if (!room.sentence) return this.errorReply("There is not a sentence game in this room.");
-		delete room.sentence;
-		room.addRaw('<div class="sentence-container"><font size="3">The Sentence Game was ended by ' + Server.nameColor(user.userid, true) + '.</font></div>');
+		"": "help",
+		help: function () {
+			this.parse(`/help sentence`);
+		},
 	},
 
 	sentencehelp: [
-		"- /sentence [length] - Begins a game of sentence game.",
-		"- /addword [word] - Adds a word to the sentence.",
-		"- /endsentence - Ends sentence game.",
+		`/sentence new [word count] - Creates a Sentence Game in the room with [word count] words as the limit. Requires %, @, &, #, ~
+		/sentence addword [word] - Adds [word] to the sentence.
+		/sentence info - Shows you the current sentence, the progress until completion and who added the last word.
+		/sentence end - Forcefully ends the Sentence Game in the room. Requires %, @, &, #, ~
+		/sentence help - Displays a list of Sentence Game commands.`,
 	],
 };

8c2a176d22b32fed1b724ee6dbcd45b9cf6818a5
returningavenger <miguelmayers2001@gmail.com>
2018-05-05 08:26:37 +0000

    Dice: Use template strings for various outputs & Implement Dice Log

--- server-plugins/dice.js
+++ server-plugins/dice.js
@@ -1,10 +1,10 @@
 /*Dice Game plugin by SilverTactic (Silveee)*/
 
-'use strict';
+"use strict";
 
 const INACTIVE_END_TIME = 1 * 60 * 1000; // 1 minute
-//const TAX = 0.10; // 10%
 const TAX = 0;
+const FS = require("../lib/fs.js");
 
 function diceImg(num) {
 	switch (num) {
@@ -30,37 +30,34 @@ class Dice {
 		this.bet = amount;
 		this.players = [];
 		this.timer = setTimeout(() => {
-			this.room.add('|uhtmlchange|' + this.room.diceCount + '|<div class = "infobox">(This game of dice has been ended due to inactivity.)</div>').update();
+			this.room.add(`|uhtmlchange|${this.room.diceCount}|<div class="infobox">(This game of dice has been ended due to inactivity.)</div>`).update();
 			delete this.room.dice;
 		}, INACTIVE_END_TIME);
 
 		let buck = (this.bet === 1 ? moneyName : moneyPlural);
-		this.startMessage = '<div class="infobox"><b style="font-size: 14pt; color: #24678d"><center>' + Server.nameColor(starter, true) + ' has started a game of dice for <span style = "color: green">' + amount + '</span> ' + buck + '!</center></b><br>' +
-			'<center><img style="margin-right: 30px;" src = "http://i.imgur.com/eywnpqX.png" width="80" height="80">' +
-			'<img style="transform:rotateY(180deg); margin-left: 30px;" src="http://i.imgur.com/eywnpqX.png" width="80" height="80"><br>' +
-			'<button name="send" value="/joindice">Click to join!</button></center>';
-		this.room.add('|uhtml|' + (++this.room.diceCount) + '|' + this.startMessage + '</div>').update();
+		this.startMessage = `<div class="infobox"><strong style="font-size: 14pt; color: #24678d"><center>${Server.nameColor(starter, true)} has started a game of dice for <span style = "color: green">${amount}</span> ${buck}!</center></strong><br /><center><img style="margin-right: 30px;" src = "http://i.imgur.com/eywnpqX.png" width="80" height="80"><img style="transform:rotateY(180deg); margin-left: 30px;" src="http://i.imgur.com/eywnpqX.png" width="80" height="80"><br /><button name="send" value="/joindice">Click to join!</button></center>`;
+		this.room.add(`|uhtml|${(++this.room.diceCount)}|${this.startMessage}</div>`).update();
 	}
 
 	join(user, self) {
 		if (this.players.length >= 2) return self.errorReply("Two users have already joined this game of dice.");
 		Economy.readMoney(user.userid, money => {
-			if (money < this.bet) return self.sendReply('You don\'t have enough money for this game of dice.');
-			if (this.players.includes(user)) return self.sendReply('You have already joined this game of dice.');
-			if (this.players.length && this.players[0].latestIp === user.latestIp) return self.errorReply("You have already joined this game of dice under the alt '" + this.players[0].name + "'.");
+			if (money < this.bet) return self.sendReply("You don't have enough money for this game of dice.");
+			if (this.players.includes(user)) return self.sendReply("You have already joined this game of dice.");
+			if (this.players.length && this.players[0].latestIp === user.latestIp) return self.errorReply(`You have already joined this game of dice under the alt "${this.players[0].name}".`);
 			if (this.players.length >= 2) return self.errorReply("Two users have already joined this game of dice.");
 
 			this.players.push(user);
-			this.room.add('|uhtmlchange|' + this.room.diceCount + '|' + this.startMessage + '<center>' + Server.nameColor(user.name) + ' has joined the game!</center></div>').update();
+			this.room.add(`|uhtmlchange|${this.room.diceCount}|${this.startMessage}<center>${Server.nameColor(user.name, true)} has joined the game!</center></div>`).update();
 			if (this.players.length === 2) this.play();
 		});
 	}
 
 	leave(user, self) {
-		if (!this.players.includes(user)) return self.sendReply('You haven\'t joined this game of dice yet.');
+		if (!this.players.includes(user)) return self.sendReply(`You haven't joined this game of dice yet.`);
 		if (this.players.length >= 2) return self.errorReply("You cannot leave a game of dice once it has been started.");
 		this.players.splice(this.players.indexOf(user), 1);
-		this.room.add('|uhtmlchange|' + this.room.diceCount + '|' + this.startMessage + '</div>');
+		this.room.add(`|uhtmlchange|${this.room.diceCount}|${this.startMessage}</div>`);
 	}
 
 	play() {
@@ -71,14 +68,14 @@ class Dice {
 				if (money1 < this.bet || money2 < this.bet) {
 					let user = (money1 < this.bet ? p1 : p2);
 					let other = (user === p1 ? p2 : p1);
-					user.sendTo(this.room, 'You have been removed from this game of dice, as you do not have enough money.');
-					other.sendTo(this.room, user.name + ' has been removed from this game of dice, as they do not have enough money. Wait for another user to join.');
+					user.sendTo(this.room, "You have been removed from this game of dice, as you do not have enough money.");
+					other.sendTo(this.room, `${user.name} has been removed from this game of dice, as they do not have enough money. Wait for another user to join.`);
 					this.players.splice(this.players.indexOf(user), 1);
-					this.room.add('|uhtmlchange|' + this.room.diceCount + '|' + this.startMessage + '<center>' + this.players.map(user => Server.nameColor(user.name)) + ' has joined the game!</center>').update();
+					this.room.add(`|uhtmlchange|${this.room.diceCount}|${this.startMessage}<center>${this.players.map(user => Server.nameColor(user.name, true))} has joined the game!</center>`).update();
 					return;
 				}
-				let players = this.players.map(user => Server.nameColor(user.name)).join(' and ');
-				this.room.add('|uhtmlchange|' + this.room.diceCount + '|' + this.startMessage + '<center>' + players + ' have joined the game!</center></div>').update();
+				let players = this.players.map(user => Server.nameColor(user.name, true)).join(" and ");
+				this.room.add(`|uhtmlchange|${this.room.diceCount}|${this.startMessage}<center>${players} have joined the game!</center></div>`).update();
 				let roll1, roll2;
 				do {
 					roll1 = Math.floor(Math.random() * 6);
@@ -90,21 +87,22 @@ class Dice {
 
 				let taxedAmt = Math.round(this.bet * TAX);
 				setTimeout(() => {
-					let buck = (this.bet === 1 ? 'buck' : 'bucks');
-					this.room.add('|uhtmlchange|' + this.room.diceCount + '|<div class="infobox"><center>' + players + ' have joined the game!<br /><br />' +
-						'The game has been started! Rolling the dice...<br />' +
-						'<img src = "' + diceImg(roll1) + '" align = "left" title = "' + Server.nameColor(p1.name) + '\'s roll"><img src = "' + diceImg(roll2) + '" align = "right" title = "' + Server.nameColor(p2.name) + '\'s roll"><br />' +
-						Server.nameColor(p1.name, true) + ' rolled ' + (roll1 + 1) + '!<br />' +
-						Server.nameColor(p2.name, true) + ' rolled ' + (roll2 + 1) + '!<br />' +
-						Server.nameColor(winner.name, true) + ' has won <b style="color:green">' + (this.bet - taxedAmt) + '</b> ' + buck + '!<br />' +
-						'Better luck next time, ' + Server.nameColor(loser.name) + '!'
-					).update();
+					let buck = (this.bet === 1 ? moneyName : moneyPlural);
+					let msg = `<div class="infobox"><center>${players} have joined the game!<br /><br />`;
+					msg += `The game has been started! Rolling the dice...<br />`;
+					msg += `<img src = "${diceImg(roll1)}" align = "left" title = "${Server.nameColor(p1.name, true)}'s roll">`;
+					msg += `<img src = "${diceImg(roll2)}" align = "right" title = "${Server.nameColor(p2.name, true)}'s roll"><br />`;
+					msg += `${Server.nameColor(p1.name, true)} rolled ${(roll1 + 1)}!<br />`;
+					msg += `${Server.nameColor(p2.name, true)} rolled ${(roll2 + 1)}!<br />`;
+					msg += `${Server.nameColor(winner.name, true)} has won <strong style="color: green">${(this.bet - taxedAmt)}</strong> ${buck}!<br />`;
+					msg += `Better luck next time, ${Server.nameColor(loser.name, true)}!`;
+					this.room.add(`|uhtmlchange|${this.room.diceCount}|${msg}`).update();
 					Economy.writeMoney(winner.userid, (this.bet - taxedAmt), () => {
 						Economy.writeMoney(loser.userid, -this.bet, () => {
 							Economy.readMoney(winner.userid, winnerMoney => {
 								Economy.readMoney(loser.userid, loserMoney => {
-									Economy.logDice(winner.userid + " has won a dice against " + loser.userid + ". They now have " + winnerMoney + (winnerMoney === 1 ? " buck." : " bucks."));
-									Economy.logDice(loser.userid + " has lost a dice against " + winner.userid + ". They now have " + loserMoney + (loserMoney === 1 ? " buck." : " bucks."));
+									Economy.logDice(`${winner.userid} has won a dice against ${loser.userid}. They now have ${winnerMoney} ${(winnerMoney === 1 ? moneyName : moneyPlural)}.`);
+									Economy.logDice(`${loser.userid} has lost a dice against ${winner.userid}. They now have ${loserMoney} ${(loserMoney === 1 ? moneyName : moneyPlural)}.`);
 									this.end();
 								});
 							});
@@ -116,60 +114,90 @@ class Dice {
 	}
 
 	end(user) {
-		if (user) this.room.add('|uhtmlchange|' + this.room.diceCount + '|<div class = "infobox">(This game of dice has been forcibly ended by ' + Server.nameColor(user.name) + '.)</div>').update();
+		if (user) this.room.add(`|uhtmlchange|${this.room.diceCount}|<div class="infobox">(This game of dice has been forcibly ended by ${Server.nameColor(user.name, true, true)}.)</div>`).update();
 		clearTimeout(this.timer);
 		delete this.room.dice;
 	}
 }
 
 exports.commands = {
-	startdice: 'dicegame',
+	startdice: "dicegame",
 	dicegame: function (target, room, user) {
-		if (room.id === 'lobby') return this.errorReply("This command cannot be used in the Lobby.");
-		if (!user.can('broadcast', null, room) && room.id !== 'casino') return this.errorReply("You must be ranked + or higher in this room to start a game of dice outside the Casino.");
+		if (room.id === "lobby") return this.errorReply("This command cannot be used in the Lobby.");
+		if (!user.can("broadcast", null, room) && room.id !== "casino") return this.errorReply("You must be ranked + or higher in this room to start a game of dice outside the Casino.");
 		if (!this.canTalk()) return;
 		if (room.dice) return this.errorReply("There is already a game of dice going on in this room.");
 
 		let amount = Number(target) || 1;
-		if (isNaN(target)) return this.errorReply('"' + target + '" isn\'t a valid number.');
-		if (target.includes('.') || amount < 1 || amount > 5000) return this.sendReply('The number of ' + moneyPlural + ' must be between 1 and 5,000 and cannot contain a decimal.');
+		if (isNaN(target)) return this.errorReply(`"${target}" isn't a valid number.`);
+		if (target.includes(".") || amount < 1 || amount > 5000) return this.errorReply(`The number of ${moneyPlural} must be between 1 and 5,000 and cannot contain a decimal.`);
 		Economy.readMoney(user.userid, money => {
-			if (money < amount) return this.sendReply("You don't have " + amount + " " + ((money === 1) ? " " + moneyName + "." : " " + moneyPlural + "."));
+			if (money < amount) return this.errorReply(`You don't have ${amount} ${money === 1 ? moneyName : moneyPlural}.`);
 			room.dice = new Dice(room, amount, user.name);
 			this.parse("/joindice");
 		});
 	},
 
-	dicejoin: 'joindice',
+	dicejoin: "joindice",
 	joindice: function (target, room, user) {
-		if (room.id === 'lobby') return this.errorReply("This command cannot be used in the Lobby.");
+		if (room.id === "lobby") return this.errorReply("This command cannot be used in the Lobby.");
 		if (!this.canTalk()) return;
-		if (!room.dice) return this.errorReply('There is no game of dice going on in this room.');
+		if (!room.dice) return this.errorReply("There is no game of dice going on in this room.");
 
 		room.dice.join(user, this);
 	},
 
-	diceleave: 'leavedice',
+	diceleave: "leavedice",
 	leavedice: function (target, room, user) {
-		if (room.id === 'lobby') return this.errorReply("This command cannot be used in the Lobby.");
-		if (!room.dice) return this.errorReply('There is no game of dice going on in this room.');
+		if (room.id === "lobby") return this.errorReply("This command cannot be used in the Lobby.");
+		if (!room.dice) return this.errorReply("There is no game of dice going on in this room.");
 
 		room.dice.leave(user, this);
 	},
 
-	diceend: 'enddice',
+	diceend: "enddice",
 	enddice: function (target, room, user) {
-		if (room.id === 'lobby') return this.errorReply("This command cannot be used in the Lobby.");
+		if (room.id === "lobby") return this.errorReply("This command cannot be used in the Lobby.");
 		if (!this.canTalk()) return;
-		if (!room.dice) return this.errorReply('There is no game of dice going on in this room.');
-		if (!user.can('broadcast', null, room) && !room.dice.players.includes(user)) return this.errorReply("You must be ranked + or higher in this room to end a game of dice.");
+		if (!room.dice) return this.errorReply("There is no game of dice going on in this room.");
+		if (!user.can("broadcast", null, room) && !room.dice.players.includes(user)) return this.errorReply("You must be ranked + or higher in this room to end a game of dice.");
 
 		room.dice.end(user);
 	},
+
+	viewdice: "dicelog",
+	dicelog: function (target, room, user) {
+		if (!this.can("money")) return false;
+		if (!target) return this.sendReply("Usage: /dicelog [number] to view the last x lines OR /dicelog [text] to search for text.");
+		let word = false;
+		if (isNaN(Number(target))) word = true;
+		let lines = FS("logs/dice.log").readIfExistsSync().split("\n").reverse();
+		let output = "";
+		let count = 0;
+		let regex = new RegExp(target.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), "gi"); // eslint-disable-line no-useless-escape
+
+		if (word) {
+			output += `Displaying last 50 lines containing "${target}":\n`;
+			for (let line in lines) {
+				if (count >= 50) break;
+				if (!~lines[line].search(regex)) continue;
+				output += `${lines[line]}\n`;
+				count++;
+			}
+		} else {
+			if (target > 100) target = 100;
+			output = lines.slice(0, (lines.length > target ? target : lines.length));
+			output.unshift(`Displaying the last ${lines.length > target ? target : lines.length} lines:`);
+			output = output.join(`\n`);
+		}
+		user.popup(`|wide|${output}`);
+	},
+
 	dicegamehelp: [
-		'/startdice or /dicegame [amount] - Starts a game of dice in the room for a given number of bucks, 1 by default (NOTE: There is a 10% tax on bucks you win from dice games).',
-		'/joindice - Joins the game of dice. You cannot use this command if you don\'t have the number of bucks the game is for.',
-		'/leavedice - Leaves the game of dice.',
-		'/enddice - Ends the game of dice. Requires + or higher to use.',
+		`/startdice or /dicegame [amount] - Starts a game of dice in the room for a given number of ${moneyPlural}, 1 ${moneyName} by default.
+		/joindice - Joins the game of dice. You cannot use this command if you don't have the number of ${moneyPlural} the game is for.
+		/leavedice - Leaves the game of dice.
+		/enddice - Ends the game of dice. Requires + or higher to use.
+		/dicelog - Views Dice-related logs. Requires @, &, ~`,
 	],
 };
